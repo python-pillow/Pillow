c144108396ba587ea9c778ffec9fe3a2a3b3a725
Stéphane Klein <stephane@harobed.org>
2011-07-20 15:49:37 +0200

    Fix : resize need int values, append int conversion in thumbnail method

--- PIL/Image.py
+++ PIL/Image.py
@@ -1547,8 +1547,8 @@ class Image:
 
         # preserve aspect ratio
         x, y = self.size
-        if x > size[0]: y = max(y * size[0] / x, 1); x = size[0]
-        if y > size[1]: x = max(x * size[1] / y, 1); y = size[1]
+        if x > size[0]: y = int(max(y * size[0] / x, 1)); x = int(size[0])
+        if y > size[1]: x = int(max(x * size[1] / y, 1)); y = int(size[1])
         size = x, y
 
         if size == self.size:
