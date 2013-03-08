0c18a8f3b8320ff57e0ca5a6386ce28de368f1b3
Alex Clark <aclark@aclark.net>
2013-03-08 08:01:56 -0500

    Use bytes literal, fixes Tests/test_file_fli.py

--- PIL/FliImagePlugin.py
+++ PIL/FliImagePlugin.py
@@ -47,7 +47,7 @@ class FliImageFile(ImageFile.ImageFile):
         magic = i16(s[4:6])
         if not (magic in [0xAF11, 0xAF12] and
                 i16(s[14:16]) in [0, 3] and  # flags
-                s[20:22] == '\x00\x00'):  # reserved
+                s[20:22] == b"\x00\x00"): # reserved
             raise SyntaxError("not an FLI/FLC file")
 
         # image characteristics
