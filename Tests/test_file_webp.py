d678b97444b8a442b19cf842a6b721f54f02849b
wiredfool <eric-github@soroos.net>
2013-03-19 13:49:01 -0700

    added write verification test

--- Tests/test_file_webp.py
+++ Tests/test_file_webp.py
@@ -3,7 +3,8 @@ from tester import *
 from PIL import Image
 
 def test_read():
-
+    """ Can we write a webp without error. Does it have the bits we expect?"""
+    
     file = "Images/lena.webp"
     im = Image.open(file)
 
@@ -21,18 +22,22 @@ def test_read():
     
 
 def test_write():
+    """ Can we write a webp without error. Does it have the bits we expect?"""
+
     file = tempfile("temp.webp")
 
     lena("RGB").save(file)
 
-    reloaded= Image.open(file)
-    reloaded.load()
+    im= Image.open(file)
+    im.load()
 
-    assert_equal(reloaded.mode, "RGB")
-    assert_equal(reloaded.size, (128, 128))
-    assert_equal(reloaded.format, "WEBP")
-    assert_no_exception(lambda: reloaded.load())
-    assert_no_exception(lambda: reloaded.getdata())
+    assert_equal(im.mode, "RGB")
+    assert_equal(im.size, (128, 128))
+    assert_equal(im.format, "WEBP")
+    assert_no_exception(lambda: im.load())
+    assert_no_exception(lambda: im.getdata())
 
-
+    # generated with: dwebp -ppm temp.webp -o lena_webp_write.ppm
+    target = Image.open('Tests/images/lena_webp_write.ppm')
+    assert_image_equal(im, target)
 
