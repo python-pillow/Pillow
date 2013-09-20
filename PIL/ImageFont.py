02855a86c07afadf380c018152197f6b6dbae5c6
paddywwoof <patrick@eldwick.org.uk>
2013-09-20 14:02:40 +0100

    hopefully this uses a reasonable y offset

--- PIL/ImageFont.py
+++ PIL/ImageFont.py
@@ -157,6 +157,9 @@ class FreeTypeFont:
     def getsize(self, text):
         return self.font.getsize(text)[0]
 
+    def getoffset(self, text):
+        return self.font.getsize(text)[1]
+
     def getmask(self, text, mode=""):
         return self.getmask2(text, mode)[0]
 
@@ -183,7 +186,7 @@ class TransposedFont:
         self.orientation = orientation # any 'transpose' argument, or None
 
     def getsize(self, text):
-        w, h = self.font.getsize(text)
+        w, h = self.font.getsize(text)[0]
         if self.orientation in (Image.ROTATE_90, Image.ROTATE_270):
             return h, w
         return w, h
