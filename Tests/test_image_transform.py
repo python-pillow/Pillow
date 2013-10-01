2241f744d99302681cbf9aed67136a38d9e28eed
wiredfool <eric-github@soroos.net>
2013-09-30 21:57:19 -0700

    extents test

--- Tests/test_image_transform.py
+++ Tests/test_image_transform.py
@@ -2,6 +2,19 @@ from tester import *
 
 from PIL import Image
 
+def test_extent():
+    im = lena('RGB')
+    (w,h) = im.size
+    transformed = im.transform(im.size, Image.EXTENT,
+                               (0,0,
+                                w/2,h/2), # ul -> lr
+                               Image.BILINEAR)
+
+    
+    scaled = im.resize((w*2, h*2), Image.BILINEAR).crop((0,0,w,h))
+    
+    assert_image_similar(transformed, scaled, 10) # undone -- precision?
+
 def test_quad():
     # one simple quad transform, equivalent to scale & crop upper left quad
     im = lena('RGB')
