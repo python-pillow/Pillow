7659ec905db9240d88f10c7d602f9f663e741499
Steve Johnson <steve@graffitilabs.com>
2013-10-06 22:43:32 -0700

    Remove plugins from built docs for now

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'plugins.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -243,4 +243,4 @@ def setup(app):
 skip_api_docs = False
 
 if skip_api_docs:
-    exclude_patterns = ['_build', 'PIL.rst', 'plugins.rst']
+    exclude_patterns += ['PIL.rst']
