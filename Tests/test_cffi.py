de538897115e0e3948caf4c42ac41e1f1876a173
wiredfool <eric-github@soroos.net>
2014-01-05 20:50:05 -0800

    removed initial testing

--- Tests/test_cffi.py
+++ Tests/test_cffi.py
@@ -22,27 +22,3 @@ def test_get():
     get.test_image()
 
 
-def xtest_direct():
-    im = Image.open('lena.png')
-    caccess = im.im.pixel_access(false)
-    access = PyAccess.new(im)
-
-    print (caccess[(0,0)])
-    print (access[(0,0)])
-    
-    print (access.im.depth)
-    print (access.im.image32[0][0])
-    print (im.getpixel((0,0)))
-    print (access.get_pixel(0,0))
-    access.set_pixel(0,0,(1,2,3))
-    print (im.getpixel((0,0)))
-    print (access.get_pixel(0,0))
-
-    access.set_pixel(0,0,(1,2,3))
-    print (im.getpixel((0,0)))
-    print (access.get_pixel(0,0))
-    
-    p_int = (5 << 24) + (4<<16) + (3 <<8)
-    access.set_pixel(0,0,p_int)
-    print (im.getpixel((0,0)))
-    print (access.get_pixel(0,0))
