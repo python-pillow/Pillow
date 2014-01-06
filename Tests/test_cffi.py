273a5014d2b3782819d7eaae144bfe80496f5125
wiredfool <eric-github@soroos.net>
2014-01-05 21:18:53 -0800

    more extensive testing, matches original c code

--- Tests/test_cffi.py
+++ Tests/test_cffi.py
@@ -21,4 +21,44 @@ def test_get():
     get.test_pixel()
     get.test_image()
 
+def _test_get_access(im):
+    """ Do we get the same thing as the old pixel access """
 
+    """ Using private interfaces, forcing a capi access and a pyaccess for the same image """
+    caccess = im.im.pixel_access(False)
+    access = PyAccess.new(im, False)
+
+    w,h = im.size
+    for x in range(0,w,10):
+        for y in range(0,h,10):
+            assert_equal(caccess[(x,y)], access[(x,y)])
+
+def test_get_vs_c():
+    _test_get_access(lena('RGB'))
+    _test_get_access(lena('RGBA'))
+    _test_get_access(lena('L'))
+    _test_get_access(lena('1'))
+    _test_get_access(lena('P'))
+
+
+def _test_set_access(im, color):
+    """ Are we writing the correct bits into the image? """
+
+    """ Using private interfaces, forcing a capi access and a pyaccess for the same image """
+    caccess = im.im.pixel_access(False)
+    access = PyAccess.new(im, False)
+
+    w,h = im.size
+    for x in range(0,w,10):
+        for y in range(0,h,10):
+            access[(x,y)] = color
+            assert_equal(caccess[(x,y)], color)
+
+def test_set_vs_c():
+    _test_set_access(lena('RGB'), (255, 128,0) )
+    _test_set_access(lena('RGBA'), (255, 192, 128, 0))
+    _test_set_access(lena('L'), 128)
+    _test_set_access(lena('1'), 255)
+    _test_set_access(lena('P') , 128)
+
+    
