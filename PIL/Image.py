37f4aea0d089dab4f396ce37e908900de2e09ec3
David Schmidt <david.schmiddi.86@gmail.com>
2014-02-25 09:56:49 +0100

    fixes #529
    doc fix

--- PIL/Image.py
+++ PIL/Image.py
@@ -1098,7 +1098,6 @@ class Image:
            third, the box defaults to (0, 0), and the second argument
            is interpreted as a mask image.
         :param mask: An optional mask image.
-        :returns: An :py:class:`~PIL.Image.Image` object.
         """
 
         if isImageType(box) and mask is None:
