7de14a51dda40f46711f8c20c6ddce6a8bac7713
Terseus <terseus@gmail.com>
2014-04-04 10:27:08 +0200

    Added base method and constants to test_imagedraw
    The method `create_base_image_draw` will be used in all the new draw
    tests.

--- Tests/test_imagedraw.py
+++ Tests/test_imagedraw.py
@@ -2,6 +2,13 @@ from tester import *
 
 from PIL import Image
 from PIL import ImageDraw
+import os.path
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (190, 190, 190)
+DEFAULT_MODE = 'RGB'
+IMAGES_PATH = os.path.join('Tests', 'images', 'imagedraw')
 
 def test_sanity():
 
@@ -25,4 +32,13 @@ def test_deprecated():
 
     assert_warning(DeprecationWarning, lambda: draw.setink(0))
     assert_warning(DeprecationWarning, lambda: draw.setfill(0))
+
+
+def create_base_image_draw(size, mode=DEFAULT_MODE, background1=WHITE, background2=GRAY):
+    img = Image.new(mode, size, background1)
+    for x in range(0, size[0]):
+        for y in range(0, size[1]):
+            if (x + y) % 2 == 0:
+                img.putpixel((x, y), background2)
+    return (img, ImageDraw.Draw(img))
 
