c15601e0b06f5d6ebf28a0f9d6a9721fa54ed235
hugovk <hugovk@users.noreply.github.com>
2014-05-11 17:16:13 +0300

    Add some more pdf tests

--- Tests/test_file_pdf.py
+++ Tests/test_file_pdf.py
@@ -2,10 +2,10 @@ from tester import *
 import os.path
 
 
-def test_to_pdf():
+def helper_save_as_pdf(mode):
     # Arrange
-    im = lena()
-    outfile = tempfile("temp.pdf")
+    im = lena(mode)
+    outfile = tempfile("temp_" + mode + ".pdf")
 
     # Act
     im.save(outfile)
@@ -13,5 +13,38 @@ def test_to_pdf():
     # Assert
     assert_true(os.path.isfile(outfile))
     assert_greater(os.path.getsize(outfile), 0)
+
+
+def test_greyscale():
+    # Arrange
+    mode = "L"
+
+    # Act / Assert
+    helper_save_as_pdf(mode)
+
+
+def test_rgb():
+    # Arrange
+    mode = "RGB"
+
+    # Act / Assert
+    helper_save_as_pdf(mode)
+
+
+def test_p_mode():
+    # Arrange
+    mode = "P"
+
+    # Act / Assert
+    helper_save_as_pdf(mode)
+
+
+def test_cmyk_mode():
+    # Arrange
+    mode = "P"
+
+    # Act / Assert
+    helper_save_as_pdf(mode)
+
 
 # End of file
