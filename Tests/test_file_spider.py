14c8d8afe4d01275d2d8973e53c7ab4e32fd3d31
hugovk <hugovk@users.noreply.github.com>
2014-05-26 21:00:20 +0300

    More spider tests

--- Tests/test_file_spider.py
+++ Tests/test_file_spider.py
@@ -1,6 +1,7 @@
 from tester import *
 
 from PIL import Image
+from PIL import SpiderImagePlugin
 
 test_file = "Tests/images/lena.spider"
 
@@ -11,5 +12,25 @@ def test_sanity():
     assert_equal(im.mode, "F")
     assert_equal(im.size, (128, 128))
     assert_equal(im.format, "SPIDER")
+
+
+def test_save():
+    # Arrange
+    temp = tempfile('temp.spider')
+    im = lena()
+
+    # Act
+    im.save(temp, "SPIDER")
+
+    # Assert
+    im2 = Image.open(temp)
+    assert_equal(im2.mode, "F")
+    assert_equal(im2.size, (128, 128))
+    assert_equal(im2.format, "SPIDER")
+
+
+def test_isSpiderImage():
+    assert_true(SpiderImagePlugin.isSpiderImage(test_file))
+
 
 # End of file
