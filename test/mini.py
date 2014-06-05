8a870c0ee9af404685c3cbf2787169b00d99d397
hugovk <hugovk@users.noreply.github.com>
2014-06-05 16:13:58 +0300

    Convert more tests

---
+++ test/mini.py
@@ -0,0 +1,5 @@
+from test_image_putpixel import TestImagePutPixel as put
+from test_image_getpixel import TestImageGetPixel as get
+
+dir(get)
+get.test_basic()
---
+++ test/test_file_psd.py
@@ -0,0 +1,23 @@
+from helper import unittest, PillowTestCase
+
+from PIL import Image
+
+# sample ppm stream
+file = "Images/lena.psd"
+data = open(file, "rb").read()
+
+
+class TestImagePsd(PillowTestCase):
+
+    def test_sanity(self):
+        im = Image.open(file)
+        im.load()
+        self.assertEqual(im.mode, "RGB")
+        self.assertEqual(im.size, (128, 128))
+        self.assertEqual(im.format, "PSD")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# End of file
