aec0dda55a6751a8d927d3c4e9cb5558eafd8793
James Anderson <james.r.andreson@utah.edu>
2014-06-09 11:39:40 -0700

    Issue #697.  On MSVC 2012 builds <unistd.h> is not required.

--- libImaging/TiffDecode.h
+++ libImaging/TiffDecode.h
@@ -13,10 +13,11 @@
 #include <tiff.h>
 #endif
 
-#ifndef _UNISTD_H
-#include <unistd.h>
+#ifndef _MSC_VER
+	#ifndef _UNISTD_H
+	#include <unistd.h>
+	#endif
 #endif
-
 
 #ifndef min
 #define min(x,y) (( x > y ) ? y : x )
