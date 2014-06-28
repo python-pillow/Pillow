d5bb962f83673f1b498a7ded1a30379d277c2c9a
hugovk <hugovk@users.noreply.github.com>
2014-06-28 23:48:14 +0300

    Add test to ensure a Pyroma is a 10/10 Mascarpone

---
+++ Tests/test_pyroma.py
@@ -0,0 +1,33 @@
+import unittest
+
+try:
+    import pyroma
+except ImportError:
+    # Skip via setUp()
+    pass
+
+
+class TestPyroma(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            import pyroma
+        except ImportError:
+            self.skipTest("ImportError")
+
+    def test_pyroma(self):
+        # Arrange
+        data = pyroma.projectdata.get_data(".")
+
+        # Act
+        rating = pyroma.ratings.rate(data)
+
+        # Assert
+        # Should have a perfect score
+        self.assertEqual(rating, (10, []))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# End of file
