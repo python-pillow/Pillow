b6f836b5b8fdb5884172e87d15fa443bdd80a058
hugovk <hugovk@users.noreply.github.com>
2014-07-01 22:53:30 +0300

    Refactor and add two more tests

--- Tests/test_file_palm.py
+++ Tests/test_file_palm.py
@@ -5,10 +5,10 @@ import os.path
 
 class TestFilePalm(PillowTestCase):
 
-    def test_save_palm_p(self):
+    def helper_save_as_palm(self, mode):
         # Arrange
-        im = lena("P")
-        outfile = self.tempfile('temp_p.palm')
+        im = lena(mode)
+        outfile = self.tempfile("temp_" + mode + ".palm")
 
         # Act
         im.save(outfile)
@@ -16,6 +16,27 @@ class TestFilePalm(PillowTestCase):
         # Assert
         self.assertTrue(os.path.isfile(outfile))
         self.assertGreater(os.path.getsize(outfile), 0)
+
+    def test_monochrome(self):
+        # Arrange
+        mode = "1"
+
+        # Act / Assert
+        self.helper_save_as_palm(mode)
+
+    def test_p_mode(self):
+        # Arrange
+        mode = "P"
+
+        # Act / Assert
+        self.helper_save_as_palm(mode)
+
+    def test_rgb_ioerror(self):
+        # Arrange
+        mode = "RGB"
+
+        # Act / Assert
+        self.assertRaises(IOError, lambda: self.helper_save_as_palm(mode))
 
 
 if __name__ == '__main__':
