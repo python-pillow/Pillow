074951f091108ec1edc0cd2c2c165be428ad86cb
Matthew Brett <matthew.brett@gmail.com>
2014-07-04 12:55:11 +0100

    RF: refactor library_installers, add zlib

--- run_install.sh
+++ run_install.sh
@@ -1,20 +1,32 @@
 source terryfy/travis_tools.sh
-source library_installers.sh
+source terryfy/library_installers.sh
+
+# Package versions for fresh source builds
+FT_VERSION=2.5.3
+PNG_VERSION=1.6.12
+ZLIB_VERSION=1.2.8
+JPEG_VERSION=9a
+OPENJPEG_VERSION=2.0.0
+TIFF_VERSION=4.0.3
+LCMS_VERSION=2.6
+WEBP_VERSION=0.4.0
 
 # Need cmake for openjpeg
 brew install cmake
 # Need pkg-config for freetype to find libpng
 brew install pkg-config
 # Set up build
-init_vars
 clean_builds
-install_jpeg
-install_tiff
-install_libpng
-install_lcms2
-install_webp
-install_openjpeg
+clean_submodule Pillow
+standard_install zlib $ZLIB_VERSION .tar.xz
+standard_install jpeg $JPEG_VERSION .tar.gz jpegsrc.v
+standard_install tiff $TIFF_VERSION
+standard_install libpng $PNG_VERSION
+standard_install lcms2 $LCMS_VERSION
+WEBP_EXTRAS="--enable-libwebpmux --enable-libwebpdemux"
+standard_install libwebp $WEBP_VERSION .tar.gz libwebp- "$WEBP_EXTRAS"
+standard_install openjpeg $OPENJPEG_VERSION .tar.gz openjpeg- cmake
 # Fix openjpeg library install id
 # https://code.google.com/p/openjpeg/issues/detail?id=367
 install_name_tool -id $PWD/build/lib/libopenjp2.6.dylib build/lib/libopenjp2.2.0.0.dylib
-install_freetype
+standard_install freetype $FT_VERSION .tar.gz freetype- "--with-harfbuzz=no"
