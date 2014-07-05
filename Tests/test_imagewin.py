315134eff38ab3a3e253d4633368a77bef848ece
hugovk <hugovk@users.noreply.github.com>
2014-07-06 00:40:27 +0300

    Test ImageWin. Not much to test, as it's mostly Windows-only.

--- Tests/test_imagewin.py
+++ Tests/test_imagewin.py
@@ -1,15 +1,35 @@
-from helper import unittest, PillowTestCase, tearDownModule, lena
+from helper import unittest, PillowTestCase, tearDownModule
 
-from PIL import Image
 from PIL import ImageWin
 
 
 class TestImageWin(PillowTestCase):
 
     def test_sanity(self):
-        dir(Image)
         dir(ImageWin)
         pass
+
+    def test_hdc(self):
+        # Arrange
+        dc = 50
+
+        # Act
+        hdc = ImageWin.HDC(dc)
+        dc2 = int(hdc)
+
+        # Assert
+        self.assertEqual(dc2, 50)
+
+    def test_hwnd(self):
+        # Arrange
+        wnd = 50
+
+        # Act
+        hwnd = ImageWin.HWND(wnd)
+        wnd2 = int(hwnd)
+
+        # Assert
+        self.assertEqual(wnd2, 50)
 
 
 if __name__ == '__main__':
