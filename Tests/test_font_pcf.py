6cfc2e0d9811bdff6ea83af1509025c4b31a4ad1
hugovk <hugovk@users.noreply.github.com>
2014-07-05 21:17:36 +0300

    Sanity test WIP FontFile.save2() in same way as save()

--- Tests/test_font_pcf.py
+++ Tests/test_font_pcf.py
@@ -3,40 +3,53 @@ from helper import unittest, PillowTestCase, tearDownModule
 from PIL import Image, FontFile, PcfFontFile
 from PIL import ImageFont, ImageDraw
 
-codecs = dir(Image.core)
+CODECS = dir(Image.core)
 
-fontname = "Tests/fonts/helvO18.pcf"
+FONTNAME = "Tests/fonts/helvO18.pcf"
 
-message = "hello, world"
+MESSAGE = "hello, world"
 
 
 class TestFontPcf(PillowTestCase):
 
     def setUp(self):
-        if "zip_encoder" not in codecs or "zip_decoder" not in codecs:
+        if "zip_encoder" not in CODECS or "zip_decoder" not in CODECS:
             self.skipTest("zlib support not available")
 
-    def save_font(self):
-        file = open(fontname, "rb")
+    def open_font(self):
+        file = open(FONTNAME, "rb")
         font = PcfFontFile.PcfFontFile(file)
         self.assertIsInstance(font, FontFile.FontFile)
         self.assertEqual(len([_f for _f in font.glyph if _f]), 192)
+        return font
 
+    def get_tempname(self):
         tempname = self.tempfile("temp.pil")
         self.addCleanup(self.delete_tempfile, tempname[:-4]+'.pbm')
+        return tempname
+
+    def save_font(self):
+        font = self.open_font()
+        tempname = self.get_tempname()
         font.save(tempname)
         return tempname
 
+    def save_font2(self):
+        font = self.open_font()
+        tempname = self.get_tempname()
+        font.save2(tempname)
+
     def test_sanity(self):
         self.save_font()
+        self.save_font2()
 
     def xtest_draw(self):
 
         tempname = self.save_font()
         font = ImageFont.load(tempname)
-        image = Image.new("L", font.getsize(message), "white")
+        image = Image.new("L", font.getsize(MESSAGE), "white")
         draw = ImageDraw.Draw(image)
-        draw.text((0, 0), message, font=font)
+        draw.text((0, 0), MESSAGE, font=font)
         # assert_signature(image, "7216c60f988dea43a46bb68321e3c1b03ec62aee")
 
     def _test_high_characters(self, message):
