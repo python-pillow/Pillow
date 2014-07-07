0a160e44d8f345d89527995bf565101a4f89fba8
wiredfool <eric-github@soroos.net>
2014-07-07 11:22:11 -0700

    Guard to prevent 64 bit machines from crashing

--- Tests/32bit_segfault_check.py
+++ Tests/32bit_segfault_check.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from PIL import Image
+import sys
 
-im = Image.new('L', (999999, 999999), 0)
+
+if sys.maxsize < 2**32:
+    im = Image.new('L', (999999, 999999), 0)
 		
 
