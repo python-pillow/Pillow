add45b494a6a99cdc9084f6b0ef8dafa66b20a8c
hugovk <hugovk@users.noreply.github.com>
2014-07-07 22:31:20 +0300

    Extract __main__ section of PIL/ImageFont.py into Scripts/createfontdatachunk.py

--- PIL/ImageFont.py
+++ PIL/ImageFont.py
@@ -403,15 +403,4 @@ w7IkEbzhVQAAAABJRU5ErkJggg==
 '''))))
     return f
 
-
-if __name__ == "__main__":
-    # create font data chunk for embedding
-    import base64
-    font = "Tests/images/courB08"
-    print("    f._load_pilfont_data(")
-    print("         # %s" % os.path.basename(font))
-    print("         BytesIO(base64.decodestring(b'''")
-    base64.encode(open(font + ".pil", "rb"), sys.stdout)
-    print("''')), Image.open(BytesIO(base64.decodestring(b'''")
-    base64.encode(open(font + ".pbm", "rb"), sys.stdout)
-    print("'''))))")
+# End of file
