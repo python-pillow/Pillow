15c92359ccf750d79b3474b95aa815149564f15f
hugovk <hugovk@users.noreply.github.com>
2014-07-20 00:51:26 +0300

    Merge some similar tests to a single file

--- Tests/test_image_mode.py
+++ Tests/test_image_mode.py
@@ -10,6 +10,27 @@ class TestImageMode(PillowTestCase):
         im = lena()
         im.mode
 
+        from PIL import ImageMode
+
+        ImageMode.getmode("1")
+        ImageMode.getmode("L")
+        ImageMode.getmode("P")
+        ImageMode.getmode("RGB")
+        ImageMode.getmode("I")
+        ImageMode.getmode("F")
+
+        m = ImageMode.getmode("1")
+        self.assertEqual(m.mode, "1")
+        self.assertEqual(m.bands, ("1",))
+        self.assertEqual(m.basemode, "L")
+        self.assertEqual(m.basetype, "L")
+
+        m = ImageMode.getmode("RGB")
+        self.assertEqual(m.mode, "RGB")
+        self.assertEqual(m.bands, ("R", "G", "B"))
+        self.assertEqual(m.basemode, "RGB")
+        self.assertEqual(m.basetype, "L")
+
     def test_properties(self):
         def check(mode, *result):
             signature = (
