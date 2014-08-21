d0ec73602b08edad601544fd44b5cf87230ada83
hugovk <hugovk@users.noreply.github.com>
2014-08-21 14:50:11 +0300

    Make OpenJPEG with 4 job slots - ~2.2 times faster (take 2)

--- depends/install_openjpeg.sh
+++ depends/install_openjpeg.sh
@@ -13,7 +13,7 @@ tar -xvzf openjpeg-2.1.0.tar.gz
 
 pushd openjpeg-2.1.0
 
-cmake -DCMAKE_INSTALL_PREFIX=/usr . && make -j 4 && sudo make -j 4 install
+cmake -DCMAKE_INSTALL_PREFIX=/usr . && make -j4 && sudo make -j4 install
 
 popd
 
