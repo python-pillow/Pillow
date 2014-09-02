afc4a26e3476344b838863ad111cd65922b7bb2f
etienne <etienne.desautels@gmail.com>
2014-09-02 14:49:24 -0400

    Fixed issue #857. When saving a JPEG and specifying 'keep' for quality or subsampling, if the source JPEG image is in grayscale mode, don't try to find the subsampling of the source, because grayscale images don't have any subsampling (it's only for color components).
    For the moment the fix also ignores subsampling of CMYK JPEG because currently Pillow doesn't support encoding JPEG in YCCK mode (and subsampling doesn't make sense in CMYK, but Pillow permits saving CMYK JPEG with subsampling, that's a bug). This fix pass those errors silently, i.e. it doesn't raise an error when 'keep' is used but it's not possible to keep the subsampling (because the image is grayscale or CMYK). I think it's the proper behavior but I'm not sure.

--- PIL/JpegImagePlugin.py
+++ PIL/JpegImagePlugin.py
@@ -545,6 +545,15 @@ def convert_dict_qtables(qtables):
 
 
 def get_sampling(im):
+    # There's no subsampling when image have only 1 layer
+    # (grayscale images) or when they are CMYK (4 layers),
+    # so set subsampling to default value.
+    #
+    # NOTE: currently Pillow can't encode JPEG to YCCK format.
+    # If YCCK support is added in the future, subsampling code will have
+    # to be updated (here and in JpegEncode.c) to deal with 4 layers.
+    if not hasattr(im, 'layers') or im.layers in (1, 4):
+        return -1
     sampling = im.layer[0][1:3] + im.layer[1][1:3] + im.layer[2][1:3]
     return samplings.get(sampling, -1)
 
