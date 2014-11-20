100309feaf59b03fa54cf637a8339b1f26abfc83
holger <h.trahe@relux.com>
2014-11-20 23:08:58 +0100

    [ADD] added PySide compability

--- PIL/ImageQt.py
+++ PIL/ImageQt.py
@@ -24,7 +24,11 @@ if 'PyQt4.QtGui' not in sys.modules:
   try:
     from PyQt5.QtGui import QImage, qRgba
   except:
-    from PyQt4.QtGui import QImage, qRgba
+    try:
+        from PyQt4.QtGui import QImage, qRgba
+    except:
+        from PySide.QtGui import QImage, qRgba
+
 else: #PyQt4 is used
   from PyQt4.QtGui import QImage, qRgba
 
