b23b3f301561caa9b10614e7a68ad5a178998fe6
hugovk <hugovk@users.noreply.github.com>
2015-01-04 11:55:06 +0200

    hugovk -> python-pillow

--- build_children.sh
+++ build_children.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Get last child project build number
-BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/hugovk/pillow-wheels/branches/latest' | grep -o '^{"branch":{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+# Get last child project build number from branch named "latest"
+BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/Pillow/pillow-wheels/branches/latest' | grep -o '^{"branch":{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+
 # Restart last child project build
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
