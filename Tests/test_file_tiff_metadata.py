47f5c9e650bf73dcc5a096d8bc931e74bd0b8060
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2015-02-23 12:11:20 +0100

    Fix float precission

--- Tests/test_file_tiff_metadata.py
+++ Tests/test_file_tiff_metadata.py
@@ -37,7 +37,8 @@ class TestFileTiffMetadata(PillowTestCase):
 
         self.assertEqual(loaded.tag[50838], (len(textdata),))
         self.assertEqual(loaded.tag[50839], textdata)
-        self.assertAlmostEqual(loaded.tag[tag_ids['RollAngle']][0], floatdata)
+        self.assertAlmostEqual(loaded.tag[tag_ids['RollAngle']][0], floatdata, 
+                               places=5)
         self.assertAlmostEqual(loaded.tag[tag_ids['YawAngle']][0], doubledata)
 
     def test_read_metadata(self):
