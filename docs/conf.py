8779b701199d9ad7205151e4720103ada83f7806
Alex Clark <aclark@aclark.net>
2015-04-04 09:40:56 -0400

    Wording [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pillow (PIL Fork)'
-copyright = u'1995-2015, Fredrik Lundh and Contributors. Alex Clark and Contributors'
-author = u'Fredrik Lundh and Contributors. Alex Clark and Contributors AKA Pillow Fighters.'
+copyright = u'1995-2015, Fredrik Lundh and Contributors, Alex Clark and Contributors'
+author = u'Fredrik Lundh and Contributors, Alex Clark and Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
