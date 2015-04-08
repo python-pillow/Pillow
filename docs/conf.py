7f4cfdf36b519fbfe405c870728f602b6ddb5d9a
Alex Clark <aclark@aclark.net>
2015-04-08 11:07:59 -0400

    Re-add extensions [ci skip]
    Fixes #1183. Thanks @joekilner. This should resolve.

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,8 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
+              'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
