dba0e7960afd59bb7348bd36260b361939901a2c
hugovk <hugovk@users.noreply.github.com>
2015-07-01 14:28:34 +0300

    More GIMP palette tests

--- Tests/test_file_gimppalette.py
+++ Tests/test_file_gimppalette.py
@@ -12,6 +12,23 @@ class TestImage(PillowTestCase):
         with open('Tests/images/hopper.jpg', 'rb') as fp:
             self.assertRaises(SyntaxError, lambda: GimpPaletteFile(fp))
 
+        with open('Tests/images/bad_palette_file.gpl', 'rb') as fp:
+            self.assertRaises(SyntaxError, lambda: GimpPaletteFile(fp))
+
+        with open('Tests/images/bad_palette_entry.gpl', 'rb') as fp:
+            self.assertRaises(ValueError, lambda: GimpPaletteFile(fp))
+
+    def test_get_palette(self):
+        # Arrange
+        with open('Tests/images/custom_gimp_palette.gpl', 'rb') as fp:
+            palette_file = GimpPaletteFile(fp)
+
+        # Act
+        palette, mode = palette_file.getpalette()
+
+        # Assert
+        self.assertEqual(mode, "RGB")
+
 
 if __name__ == '__main__':
     unittest.main()
