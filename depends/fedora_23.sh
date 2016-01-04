b952f7e3af773f024ee1da5a411dff54d80095eb
wiredfool <eric-github@soroos.net>
2016-01-04 07:42:03 -0800

    tkinter, notes

--- depends/fedora_23.sh
+++ depends/fedora_23.sh
@@ -4,6 +4,7 @@
 # Installs all of the dependencies for Pillow for Fedora 23
 # for both system Pythons 2.7 and 3.4
 #
+# note that Fedora does ship packages for Pillow as python-pillow
 
 # this is a workaround for 
 # "gcc: error: /usr/lib/rpm/redhat/redhat-hardened-cc1: No such file or directory"
@@ -12,6 +13,6 @@ sudo dnf install redhat-rpm-config
 
 sudo dnf install python-devel python3-devel python-virtualenv make gcc
 
-# Note, I can't find a python2-tkinter package
 sudo dnf install libtiff-devel libjpeg-devel libzip-devel freetype-devel \
-    lcms2-devel libwebp-devel openjpeg2-devel python3-tkinter tcl-devel tk-devel
+    lcms2-devel libwebp-devel openjpeg2-devel tkinter python3-tkinter \ 
+    tcl-devel tk-devel
