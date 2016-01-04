804063357405baed9f4e9b8a600b8839a2533f5c
wiredfool <eric-github@soroos.net>
2016-01-04 07:33:46 -0800

    Yum has been replaced

--- depends/fedora_23.sh
+++ depends/fedora_23.sh
@@ -8,10 +8,10 @@
 # this is a workaround for 
 # "gcc: error: /usr/lib/rpm/redhat/redhat-hardened-cc1: No such file or directory"
 # errors when compiling.
-sudo yum install redhat-rpm-config
+sudo dnf install redhat-rpm-config
 
-sudo yum install python-devel python3-devel python-virtualenv make gcc
+sudo dnf install python-devel python3-devel python-virtualenv make gcc
 
 # Note, I can't find a python2-tkinter package
-sudo yum install libtiff-devel libjpeg-devel libzip-devel freetype-devel \
+sudo dnf install libtiff-devel libjpeg-devel libzip-devel freetype-devel \
     lcms2-devel libwebp-devel openjpeg2-devel python3-tkinter tcl-devel tk-devel
