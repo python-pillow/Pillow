f826bff3c7f174584f85f620dccd55b26f71d5fa
hugovk <hugovk@users.noreply.github.com>
2016-01-06 11:24:09 +0200

    Simplify sanity tests

--- Tests/test_file_dds.py
+++ Tests/test_file_dds.py
@@ -11,22 +11,18 @@ class TestFileCur(PillowTestCase):
     def test_sanity_dxt1(self):
         im = Image.open(TEST_FILE_DXT1)
 
+        self.assertEqual(im.format, "DDS")
+        self.assertEqual(im.mode, "RGBA")
         self.assertEqual(im.size, (256, 256))
         self.assertIsInstance(im, DdsImagePlugin.DdsImageFile)
-        # Check some pixel colors to ensure image is loaded properly
-        self.assertEqual(im.getpixel((10, 1)), (49, 50, 53, 44))
-        self.assertEqual(im.getpixel((11, 1)), (32, 50, 53, 53))
-        self.assertEqual(im.getpixel((16, 16)), (91, 48, 44, 32))
 
     def test_sanity_dxt5(self):
         im = Image.open(TEST_FILE_DXT5)
 
+        self.assertEqual(im.format, "DDS")
+        self.assertEqual(im.mode, "RGBA")
         self.assertEqual(im.size, (256, 256))
         self.assertIsInstance(im, DdsImagePlugin.DdsImageFile)
-        # Check some pixel colors to ensure image is loaded properly
-        self.assertEqual(im.getpixel((10, 1)), (49, 50, 53, 44))
-        self.assertEqual(im.getpixel((11, 1)), (32, 49, 50, 56))
-        self.assertEqual(im.getpixel((16, 16)), (91, 48, 44, 32))
 
     def test__validate_true(self):
         # Arrange
