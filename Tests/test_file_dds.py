517cbbff08042ef8957e7a6a92bbcc1113bcb104
hugovk <hugovk@users.noreply.github.com>
2016-01-06 11:59:47 +0200

    Test NotImplementedError

--- Tests/test_file_dds.py
+++ Tests/test_file_dds.py
@@ -3,6 +3,7 @@ from helper import unittest, PillowTestCase
 from PIL import Image, DdsImagePlugin
 
 TEST_FILE_DXT1 = "Tests/images/dxt1-rgb-4bbp-noalpha_MipMaps-1.dds"
+TEST_FILE_DXT3 = "Tests/images/dxt3-argb-8bbp-explicitalpha_MipMaps-1.dds"
 TEST_FILE_DXT5 = "Tests/images/dxt5-argb-8bbp-interpolatedalpha_MipMaps-1.dds"
 
 
@@ -23,6 +24,10 @@ class TestFileCur(PillowTestCase):
         self.assertEqual(im.mode, "RGBA")
         self.assertEqual(im.size, (256, 256))
         self.assertIsInstance(im, DdsImagePlugin.DdsImageFile)
+
+    def test_sanity_dxt3(self):
+        self.assertRaises(NotImplementedError,
+                          lambda: Image.open(TEST_FILE_DXT3))
 
     def test__validate_true(self):
         # Arrange
