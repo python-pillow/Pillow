da7e712cb407911bc4766710464ad7b2c7768d46
Andrew Murray <radarhere@gmail.com>
2016-01-23 13:46:36 +1100

    Added MIC test

--- Tests/test_file_mic.py
+++ Tests/test_file_mic.py
@@ -6,10 +6,15 @@ from PIL import MicImagePlugin
 class TestFileMic(PillowTestCase):
 
     def test_invalid_file(self):
+        # Test an invalid OLE file
         invalid_file = "Tests/images/flower.jpg"
-
         self.assertRaises(SyntaxError,
                           lambda: MicImagePlugin.MicImageFile(invalid_file))
+
+        # Test a valid OLE file, but not a MIC file
+        ole_file = "Tests/images/test-ole-file.doc"
+        self.assertRaises(SyntaxError,
+                          lambda: MicImagePlugin.MicImageFile(ole_file))
 
 
 if __name__ == '__main__':
