1888d605049a3cbfe36cdf38dc79a6a239601547
Andrew Murray <radarhere@gmail.com>
2016-04-05 23:01:39 +1000

    Added PixarImagePlugin file extension

--- PIL/PixarImagePlugin.py
+++ PIL/PixarImagePlugin.py
@@ -64,5 +64,4 @@ class PixarImageFile(ImageFile.ImageFile):
 
 Image.register_open(PixarImageFile.format, PixarImageFile)
 
-#
-# FIXME: what's the standard extension?
+Image.register_extension(PixarImageFile.format, ".pxr")
