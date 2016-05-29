fa81287f2c91c5f9369b06ad320beaee0a08b7a4
wiredfool <eric-github@soroos.net>
2016-05-29 13:50:10 -0700

    Python3 support

--- decode.c
+++ decode.c
@@ -938,7 +938,7 @@ _imaging_tell_pyFd(PyObject *fd)
     Py_ssize_t location;
     
     result = PyObject_CallMethod(fd, "tell", NULL);
-    location = PyInt_AsSsize_t(result);
+    location = PyLong_AsSsize_t(result);
 
     Py_DECREF(result);
     return location;
