ca69de192b4969ad550474385d8f932575d79be7
Diederik Veeze <d.veeze@zestsoftware.nl>
2016-06-26 12:51:48 +0200

    Assign to unused layers as well, jtbs

--- libImaging/Convert.c
+++ libImaging/Convert.c
@@ -910,7 +910,7 @@ p2la(UINT8* out, const UINT8* in, int xsize, const UINT8* palette)
     /* FIXME: precalculate greyscale palette? */
     for (x = 0; x < xsize; x++, out+=4) {
         const UINT8* rgba = &palette[*in++ * 4];
-        out[0] = L(rgba) / 1000;
+        out[0] = out[1] = out[2] = L(rgba) / 1000;
         out[3] = rgba[3];
     }
 }
