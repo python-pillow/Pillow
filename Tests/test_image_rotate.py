90077b397504c19d1aaf97a9081400130ad8ce46
Marcus Brinkmann <m.brinkmann@semantics.de>
2016-10-05 22:26:26 +0200

    Add center and translate option to Image.rotate.

--- Tests/test_image_rotate.py
+++ Tests/test_image_rotate.py
@@ -5,11 +5,11 @@ from PIL import Image
 class TestImageRotate(PillowTestCase):
 
     def test_rotate(self):
-        def rotate(im, mode, angle):
-            out = im.rotate(angle)
+        def rotate(im, mode, angle, center=None, translate=None):
+            out = im.rotate(angle, center=center, translate=translate)
             self.assertEqual(out.mode, mode)
             self.assertEqual(out.size, im.size)  # default rotate clips output
-            out = im.rotate(angle, expand=1)
+            out = im.rotate(angle, center=center, translate=translate, expand=1)
             self.assertEqual(out.mode, mode)
             if angle % 180 == 0:
                 self.assertEqual(out.size, im.size)
@@ -30,6 +30,10 @@ class TestImageRotate(PillowTestCase):
         for angle in (0, 45, 90, 180, 270):
             im = Image.new('RGB',(0,0))
             rotate(im, im.mode, angle)
+
+        rotate(im, im.mode, 45, center=(0, 0))
+        rotate(im, im.mode, 45, translate=(im.size[0]/2, 0))
+        rotate(im, im.mode, 45, center=(0, 0), translate=(im.size[0]/2, 0))
 
 
 if __name__ == '__main__':
