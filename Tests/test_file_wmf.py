c7faca7e0f36789d0bbdbfee1b711be9066222f2
aagaidiu <alexey.a.gaidiukov@intel.com>
2016-11-25 15:34:39 -0800

    WMF/EMF: Unpack signed integers using unpack()

--- Tests/test_file_wmf.py
+++ Tests/test_file_wmf.py
@@ -6,11 +6,25 @@ class TestFileWmf(PillowTestCase):
 
     def test_load_raw(self):
 
-        # Test basic EMF open
+        # Test basic EMF open and rendering
         im = Image.open('Tests/images/drawing.emf')
+        if hasattr(Image.core, "drawwmf"):
+            # Currently, support for WMF/EMF is Windows-only
+            im.load()
+            # Compare to reference rendering
+            imref = Image.open('Tests/images/drawing_emf_ref.png')
+            imref.load()
+            self.assert_image_equal(im, imref)
 
-        # Test basic WMF open
+        # Test basic WMF open and rendering
         im = Image.open('Tests/images/drawing.wmf')
+        if hasattr(Image.core, "drawwmf"):
+            # Currently, support for WMF/EMF is Windows-only
+            im.load()
+            # Compare to reference rendering
+            imref = Image.open('Tests/images/drawing_wmf_ref.png')
+            imref.load()
+            self.assert_image_equal(im, imref)
 
 
 if __name__ == '__main__':
