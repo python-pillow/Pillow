d764a4c985b715c063a246bcb1fa8571e8ebb9a7
hugovk <hugovk@users.noreply.github.com>
2016-12-24 19:16:11 +0200

    Refactor to avoid repeated version number

--- depends/install_openjpeg.sh
+++ depends/install_openjpeg.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 # install openjpeg
 
+archive=openjpeg-2.1.2
 
-if [ ! -f openjpeg-2.1.2.tar.gz ]; then
-    wget -O 'openjpeg-2.1.2.tar.gz' 'https://github.com/python-pillow/pillow-depends/blob/master/openjpeg-2.1.2.tar.gz?raw=true'
+./download-and-extract.sh $archive https://github.com/python-pillow/pillow-depends/blob/master/$archive.tar.gz?raw=true
 
-fi
-
-rm -r openjpeg-2.1.2
-tar -xvzf openjpeg-2.1.2.tar.gz
-
-
-pushd openjpeg-2.1.2
+pushd $archive
 
 cmake -DCMAKE_INSTALL_PREFIX=/usr . && make -j4 && sudo make -j4 install
 
 popd
-
