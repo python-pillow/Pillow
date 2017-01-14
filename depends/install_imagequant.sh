c5cd32dd4af58492910e1af1b2d7b7653a3907da
Andrew Murray <radarhere@gmail.com>
2017-01-14 19:54:14 +1100

    Changed from pngquant to libimagequant

--- depends/install_imagequant.sh
+++ depends/install_imagequant.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # install libimagequant
 
-archive=pngquant-2.8.2
+archive=libimagequant-2.8.2
 
 ./download-and-extract.sh $archive https://raw.githubusercontent.com/python-pillow/pillow-depends/master/$archive.tar.gz
 
 pushd $archive
 
-make -C lib shared
-sudo cp lib/libimagequant.so* /usr/lib/
-sudo cp lib/libimagequant.h /usr/include/
+make shared
+sudo cp libimagequant.so* /usr/lib/
+sudo cp libimagequant.h /usr/include/
 
 popd
