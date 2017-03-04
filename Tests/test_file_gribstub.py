0abfa08ff74c075f05b19a491e01106dd1815fb4
hugovk <hugovk@users.noreply.github.com>
2017-03-04 13:07:48 +0200

    Test GribStubImagePlugin for correctness

--- Tests/test_file_gribstub.py
+++ Tests/test_file_gribstub.py
@@ -1,22 +1,46 @@
 from helper import unittest, PillowTestCase, hopper
 
-from PIL import GribStubImagePlugin
+from PIL import GribStubImagePlugin, Image
+
+TEST_FILE = "Tests/images/WAlaska.wind.7days.grb"
 
 
 class TestFileGribStub(PillowTestCase):
 
+    def test_open(self):
+        # Act
+        im = Image.open(TEST_FILE)
+
+        # Assert
+        self.assertEqual(im.format, "GRIB")
+
+        # Dummy data from the stub
+        self.assertEqual(im.mode, "F")
+        self.assertEqual(im.size, (1, 1))
+
     def test_invalid_file(self):
+        # Arrange
         invalid_file = "Tests/images/flower.jpg"
 
+        # Act / Assert
         self.assertRaises(SyntaxError,
                           lambda:
                           GribStubImagePlugin.GribStubImageFile(invalid_file))
 
     def test_save(self):
+        # Arrange
         im = hopper()
-
         tmpfile = self.tempfile("temp.grib")
+
+        # Act / Assert: stub cannot save without an implemented handler
         self.assertRaises(IOError, lambda: im.save(tmpfile))
+
+    def test_load(self):
+        # Arrange
+        im = Image.open(TEST_FILE)
+
+        # Act / Assert: stub cannot load without an implemented handler
+        self.assertRaises(IOError, lambda: im.load())
 
 
 if __name__ == '__main__':
