9bdda3d5526fbda334a9828b32bde455a56388cf
wiredfool <eric-github@soroos.net>
2017-06-13 13:22:53 -0700

    update install script

--- depends/install_raqm.sh
+++ depends/install_raqm.sh
@@ -2,16 +2,11 @@
 # install raqm
 
 
-if [ ! -f raqm-0.2.0.tar.gz ]; then
-    wget -O 'raqm-0.2.0.tar.gz' 'https://github.com/HOST-Oman/libraqm/releases/download/v0.2.0/raqm-0.2.0.tar.gz?raw=true'
+archive=raqm-0.2.0
 
-fi
+./download-and-extract.sh $archive https://raw.githubusercontent.com/python-pillow/pillow-depends/master/$archive.tar.gz
 
-rm -r raqm-0.2.0
-tar -xvzf raqm-0.2.0.tar.gz
-
-
-pushd raqm-0.2.0
+pushd $archive
 
 ./configure --prefix=/usr && make -j4 && sudo make -j4 install
 
