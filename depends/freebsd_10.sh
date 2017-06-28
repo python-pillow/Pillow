57632d56d7cf35ed69f046ac223dda5c78eca2e3
Eric Soroos <eric-github@soroos.net>
2017-06-28 21:03:51 +0000

    Use the cmake version of raqm

--- depends/freebsd_10.sh
+++ depends/freebsd_10.sh
@@ -10,4 +10,4 @@ sudo pkg install python2 python3 py27-pip py27-virtualenv wget
 # I can't find a python3.4 version of tkinter
 sudo pkg install jpeg-turbo tiff webp lcms2 freetype2 harfbuzz fribidi py27-tkinter
 
-./install_raqm.sh
+./install_raqm_cmake.sh
