109e10d6b79271fa8d9ef241574884a4896bf3b4
Eric Soroos <eric-github@soroos.net>
2017-06-28 21:29:57 +0000

    cmake dependency

--- depends/freebsd_10.sh
+++ depends/freebsd_10.sh
@@ -4,7 +4,7 @@
 # Installs all of the dependencies for Pillow for Freebsd 10.x
 # for both system Pythons 2.7 and 3.4
 #
-sudo pkg install python2 python3 py27-pip py27-virtualenv wget
+sudo pkg install python2 python3 py27-pip py27-virtualenv wget cmake
 
 # Openjpeg fails badly using the openjpeg package.
 # I can't find a python3.4 version of tkinter
