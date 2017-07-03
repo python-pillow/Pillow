bc5f9a3e8bfa5acb301f9a7cdabeb4acac040c9c
wiredfool <eric-github@soroos.net>
2017-07-03 21:51:39 +0100

    add pypy to appveyor

--- winbuild/config.py
+++ winbuild/config.py
@@ -5,6 +5,7 @@ PILLOW_DEPENDS_DIR = 'C:\\pillow-depends\\'
 
 pythons = {  # '26': 7,
            '27': 7,
+           'pypy2': 7,
            # '32': 7,
            '33': 7.1,
            '34': 7.1}
