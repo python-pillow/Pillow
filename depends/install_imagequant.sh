5aa91c3e5eeac7a4e73fc438f0b2add2ae643b39
Andrew Murray <radarhere@users.noreply.github.com>
2017-07-03 20:37:48 +1000

    Updated libimagequant to 2.10.0

--- depends/install_imagequant.sh
+++ depends/install_imagequant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install libimagequant
 
-archive=libimagequant-2.9.1
+archive=libimagequant-2.10.0
 
 ./download-and-extract.sh $archive https://raw.githubusercontent.com/python-pillow/pillow-depends/master/$archive.tar.gz
 
