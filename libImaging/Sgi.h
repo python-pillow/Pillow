8f83c3fa4af55e18c93a21452c19861cdbb2279c
Mickael B <gtmicka@hotmail.fr>
2017-07-29 02:39:37 -0400

    store SgiRleDecoder elements in SGISTATE struct

--- libImaging/Sgi.h
+++ libImaging/Sgi.h
@@ -7,4 +7,34 @@ typedef struct {
     /* Number of bytes per channel per pixel */
     int bpc;
 
+    /* RLE offsets table */
+    uint32_t *starttab;
+
+    /* RLE lengths table */
+    uint32_t *lengthtab;
+
+    /* current row offset */
+    uint32_t rleoffset;
+
+    /* current row length */
+    uint32_t rlelength;
+
+    /* RLE table size */
+    int tablen;
+
+    /* RLE table index */
+    int tabindex;
+
+    /* buffer index */
+    int bufindex;
+
+    /* current row index */
+    int rowno;
+
+    /* current channel index */
+    int channo;
+
+    /* image data size from file descriptor */
+    long bufsize;
+
 } SGISTATE;
