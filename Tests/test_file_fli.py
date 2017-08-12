7088e1a201d861da8991343924d5c41ea429ab14
Andrew Murray <radarhere@users.noreply.github.com>
2017-08-12 19:07:06 +1000

    Added tests

--- Tests/test_file_fli.py
+++ Tests/test_file_fli.py
@@ -16,6 +16,16 @@ class TestFileFli(PillowTestCase):
         self.assertEqual(im.size, (128, 128))
         self.assertEqual(im.format, "FLI")
 
+    def test_tell(self):
+        # Arrange
+        im = Image.open(test_file)
+
+        # Act
+        frame = im.tell()
+
+        # Assert
+        self.assertEqual(frame, 0)
+
     def test_invalid_file(self):
         invalid_file = "Tests/images/flower.jpg"
 
@@ -38,6 +48,15 @@ class TestFileFli(PillowTestCase):
                 break
             except EOFError:
                 self.assertLess(im.tell(), n_frames)
+
+    def test_seek_outside(self):
+        # Test negative seek
+        im = Image.open(test_file)
+        im.seek(-1)
+        self.assertEqual(im.tell(), 0)
+
+        # Test seek past end of file
+        self.assertRaises(EOFError, lambda: im.seek(2))
 
 
 if __name__ == '__main__':
