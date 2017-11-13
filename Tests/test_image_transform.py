fa9e9c2795e845a2447a2191390bffcd08a41a7e
Eric Soroos <eric-github@soroos.net>
2017-11-13 11:59:26 +0000

    remove duplicate test

--- Tests/test_image_transform.py
+++ Tests/test_image_transform.py
@@ -267,16 +267,5 @@ class TestImageTransformPerspective(TestImageTransformAffine):
     # Repeat all tests for AFFINE transformations with PERSPECTIVE
     transform = Image.PERSPECTIVE
 
-class TestFill(unittest.TestCase):
-    def test_fill(self):
-        im = hopper('RGB')
-        (w, h) = im.size
-        transformed = im.transform(im.size, Image.EXTENT,
-                                   (0, 0,
-                                    w*2, h*2),
-                                   Image.BILINEAR,
-                                   fill = 'red')
-        transformed.show()
-        self.assertEqual(transformed.getpixel((w-1,h-1)), (255,0,0))
 if __name__ == '__main__':
     unittest.main()
