4d685ac0cd5a41802a9101412bd7e82120132972
Jerome Leclanche <jerome@leclan.ch>
2018-02-18 06:24:39 +0200

    blp: Add more test images

--- Tests/test_file_blp.py
+++ Tests/test_file_blp.py
@@ -4,10 +4,40 @@ from helper import PillowTestCase, unittest
 
 
 class TestFileBlp(PillowTestCase):
+    def test_load_blp1_alpha4(self):
+        im = Image.open("Tests/images/blp/blp1_alpha4.blp")
+        target = Image.open("Tests/images/blp/blp1_alpha4.png")
+        self.assert_image_equal(im, target)
+
+    def test_load_blp1_alpha5(self):
+        im = Image.open("Tests/images/blp/blp1_alpha5.blp")
+        target = Image.open("Tests/images/blp/blp1_alpha5.png")
+        self.assert_image_equal(im, target)
+
+    def test_load_blp2_raw(self):
+        im = Image.open("Tests/images/blp/blp2_raw.blp")
+        target = Image.open("Tests/images/blp/blp2_raw.png")
+        self.assert_image_equal(im, target)
+
     def test_load_blp2_dxt1(self):
-        im = Image.open("Tests/images/blp2_dxt1.blp")
-        target = Image.open("Tests/images/blp2_dxt1.png")
-        self.assert_image_similar(im, target.convert("RGBA"), 15)
+        im = Image.open("Tests/images/blp/blp2_dxt1.blp")
+        target = Image.open("Tests/images/blp/blp2_dxt1.png")
+        self.assert_image_equal(im, target)
+
+    def test_load_blp2_dxt1a(self):
+        im = Image.open("Tests/images/blp/blp2_dxt1a.blp")
+        target = Image.open("Tests/images/blp/blp2_dxt1a.png")
+        self.assert_image_equal(im, target)
+
+    def test_load_blp2_dxt3(self):
+        im = Image.open("Tests/images/blp/blp2_dxt3.blp")
+        target = Image.open("Tests/images/blp/blp2_dxt3.png")
+        self.assert_image_equal(im, target)
+
+    def test_load_blp2_dxt5(self):
+        im = Image.open("Tests/images/blp/blp2_dxt5.blp")
+        target = Image.open("Tests/images/blp/blp2_dxt5.png")
+        self.assert_image_equal(im, target)
 
 
 if __name__ == "__main__":
