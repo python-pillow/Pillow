f826dc37d1565a71b5ee916474afa4822457a968
Alexander <homm86@gmail.com>
2018-03-25 12:54:13 +0300

    Fix incorrect image type checking in _imagingmorph module

--- src/_imagingmorph.c
+++ src/_imagingmorph.c
@@ -64,12 +64,12 @@ apply(PyObject *self, PyObject* args)
     width = imgin->xsize;
     height = imgin->ysize;
 
-    if (imgin->type != IMAGING_TYPE_UINT8 &&
+    if (imgin->type != IMAGING_TYPE_UINT8 ||
         imgin->bands != 1) {
         PyErr_SetString(PyExc_RuntimeError, "Unsupported image type");
         return NULL;
     }
-    if (imgout->type != IMAGING_TYPE_UINT8 &&
+    if (imgout->type != IMAGING_TYPE_UINT8 ||
         imgout->bands != 1) {
         PyErr_SetString(PyExc_RuntimeError, "Unsupported image type");
         return NULL;
@@ -167,7 +167,7 @@ match(PyObject *self, PyObject* args)
     lut = PyBytes_AsString(py_lut);
     imgin = (Imaging) i0;
 
-    if (imgin->type != IMAGING_TYPE_UINT8 &&
+    if (imgin->type != IMAGING_TYPE_UINT8 ||
         imgin->bands != 1) {
         PyErr_SetString(PyExc_RuntimeError, "Unsupported image type");
         return NULL;
