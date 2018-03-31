912980c52f35e1ab4453aa29b83a4ec1c3e3d861
Alexander <homm86@gmail.com>
2018-03-31 19:55:43 +0300

    Remove Color3DLUT.from_cube_file from сore library

--- src/PIL/ImageFilter.py
+++ src/PIL/ImageFilter.py
@@ -389,66 +389,6 @@ class Color3DLUT(MultibandFilter):
 
         return cls((size1D, size2D, size3D), table, channels, target_mode)
 
-    @classmethod
-    def from_cube_file(cls, lines, target_mode=None):
-        name, size = None, None
-        channels = 3
-        file = None
-
-        if isPath(lines):
-            file = lines = open(lines, 'rt')
-
-        try:
-            iterator = iter(lines)
-
-            for i, line in enumerate(iterator, 1):
-                line = line.strip()
-                if line.startswith('TITLE "'):
-                    name = line.split('"')[1]
-                    continue
-                if line.startswith('LUT_3D_SIZE '):
-                    size = [int(x) for x in line.split()[1:]]
-                    if len(size) == 1:
-                        size = size[0]
-                    continue
-                if line.startswith('CHANNELS '):
-                    channels = int(line.split()[1])
-                if line.startswith('LUT_1D_SIZE '):
-                    raise ValueError("1D LUT cube files aren't supported.")
-
-                try:
-                    float(line.partition(' ')[0])
-                except ValueError:
-                    pass
-                else:
-                    # Data starts
-                    break
-
-            if size is None:
-                raise ValueError('No size found in the file')
-
-            table = []
-            for i, line in enumerate(chain([line], iterator), i):
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-                try:
-                    pixel = [float(x) for x in line.split()]
-                except ValueError:
-                    raise ValueError("Not a number on line {}".format(i))
-                if len(pixel) != channels:
-                    raise ValueError(
-                        "Wrong number of colors on line {}".format(i))
-                table.append(tuple(pixel))
-        finally:
-            if file is not None:
-                file.close()
-
-        instance = cls(size, table, channels, target_mode)
-        if name is not None:
-            instance.name = name
-        return instance
-
     def filter(self, image):
         from . import Image
 
