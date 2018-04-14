cc70972a20ff2761b182f007d5c6582f13851023
Alexander <homm86@gmail.com>
2018-04-15 01:23:45 +0300

    install numpy on all travis machines

--- .travis/install.sh
+++ .travis/install.sh
@@ -15,6 +15,7 @@ pip install -U pytest
 pip install -U pytest-cov
 pip install pyroma
 pip install test-image-results
+pip install numpy
 
 # docs only on Python 2.7
 if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ]; then pip install -r requirements.txt ; fi
