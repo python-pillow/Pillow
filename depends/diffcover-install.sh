67563cff336e8198bae93aa48918a00943fc941c
hugovk <hugovk@users.noreply.github.com>
2018-10-17 09:23:41 +0300

    Fix 'no such option: --use-wheel'

--- depends/diffcover-install.sh
+++ depends/diffcover-install.sh
@@ -4,4 +4,4 @@ git fetch origin master:refs/remotes/origin/master
 
 # CFLAGS=-O0 means build with no optimisation.
 # Makes build much quicker for lxml and other dependencies.
-time CFLAGS=-O0 pip install --use-wheel diff_cover
+time CFLAGS=-O0 pip install diff_cover
