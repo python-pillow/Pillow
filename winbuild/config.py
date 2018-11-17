ea8a80ba311aa385c664bb7c056cb20c1759722b
Andrew Murray <radarhere@users.noreply.github.com>
2018-11-17 19:55:24 +1100

    Updated libwebp to 1.0.1

--- winbuild/config.py
+++ winbuild/config.py
@@ -66,9 +66,9 @@ libs = {
         'version': '8.6.8',
     },
     'webp': {
-        'url': 'http://downloads.webmproject.org/releases/webp/libwebp-1.0.0.tar.gz',
-        'filename': PILLOW_DEPENDS_DIR + 'libwebp-1.0.0.tar.gz',
-        'dir': 'libwebp-1.0.0',
+        'url': 'http://downloads.webmproject.org/releases/webp/libwebp-1.0.1.tar.gz',
+        'filename': PILLOW_DEPENDS_DIR + 'libwebp-1.0.1.tar.gz',
+        'dir': 'libwebp-1.0.1',
     },
     'openjpeg': {
         'url': SF_MIRROR+'/project/openjpeg/openjpeg/2.3.0/openjpeg-2.3.0.tar.gz',
