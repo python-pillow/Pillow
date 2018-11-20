84df069169b4ad429f34a6b1bc318ba9aae73129
Andrew Murray <radarhere@users.noreply.github.com>
2018-11-20 18:50:00 +1100

    Use im.info background when saving

--- src/PIL/WebPImagePlugin.py
+++ src/PIL/WebPImagePlugin.py
@@ -191,7 +191,11 @@ def _save_all(im, fp, filename):
         _save(im, fp, filename)
         return
 
-    background = encoderinfo.get("background", (0, 0, 0, 0))
+    background = (0, 0, 0, 0)
+    if "background" in encoderinfo:
+        background = encoderinfo["background"]
+    elif "background" in im.info:
+        background = im.info["background"]
     duration = im.encoderinfo.get("duration", 0)
     loop = im.encoderinfo.get("loop", 0)
     minimize_size = im.encoderinfo.get("minimize_size", False)
