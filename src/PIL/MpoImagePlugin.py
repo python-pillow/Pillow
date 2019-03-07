d0172a08ddfb65860fef917a020e12069a2be653
Hugo <hugovk@users.noreply.github.com>
2019-03-07 13:22:20 +0100

    Update src/PIL/MpoImagePlugin.py
    Co-Authored-By: Glandos <bugs-github@antipoul.fr>

--- src/PIL/MpoImagePlugin.py
+++ src/PIL/MpoImagePlugin.py
@@ -109,7 +109,7 @@ class MpoImageFile(JpegImagePlugin.JpegImageFile):
         This is essentially useful when opening a JPEG
         file that reveals itself as an MPO, to avoid
         double call to _open.
-        '''
+        """
         jpeg_instance.__class__ = MpoImageFile
         jpeg_instance._after_jpeg_open(mpheader)
         return jpeg_instance
