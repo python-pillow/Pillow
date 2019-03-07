b29365e8a01e7be1ab8cca039d4a180c6e33fae2
Andrew Murray <radarhere@users.noreply.github.com>
2019-03-07 15:24:27 +1100

    Updated list of Unix utilities used to show an image [ci skip]

--- src/PIL/Image.py
+++ src/PIL/Image.py
@@ -2021,7 +2021,7 @@ class Image(object):
         debugging purposes.
 
         On Unix platforms, this method saves the image to a temporary
-        PPM file, and calls either the **xv** utility or the **display**
+        PPM file, and calls the **display**, **eog** or **xv**
         utility, depending on which one can be found.
 
         On macOS, this method saves the image to a temporary PNG file, and
