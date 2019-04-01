4f3b44960f103bc201a742332dbf9a77de1eaa87
Andrew Murray <radarhere@users.noreply.github.com>
2019-04-02 09:25:27 +1100

    Resolved segfaults

--- src/libImaging/Convert.c
+++ src/libImaging/Convert.c
@@ -982,8 +982,8 @@ static void
 p2pa(UINT8* out, const UINT8* in, int xsize, const UINT8* palette)
 {
     int x;
-    for (x = 0; x < xsize; x++, in+=4) {
-        const UINT8* rgba = &palette[in[0] * 4];
+    for (x = 0; x < xsize; x++, in++) {
+        const UINT8* rgba = &palette[in[0]];
         *out++ = in[0];
         *out++ = in[0];
         *out++ = in[0];
