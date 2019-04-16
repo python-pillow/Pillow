332321acf334a0c779cedacc39acc2842e85ecbc
Andrew Murray <radarhere@users.noreply.github.com>
2019-04-16 20:00:11 +1000

    Updated libpng to 1.6.37

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ ARCHIVE_SDIR=pillow-depends-master
 
 # Package versions for fresh source builds
 FREETYPE_VERSION=2.10.0
-LIBPNG_VERSION=1.6.36
+LIBPNG_VERSION=1.6.37
 ZLIB_VERSION=1.2.11
 JPEG_VERSION=9c
 OPENJPEG_VERSION=2.3.1
