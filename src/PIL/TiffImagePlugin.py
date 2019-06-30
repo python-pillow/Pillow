a3520ab82068b489244567c086444722d8a65cc7
Hugo <hugovk@users.noreply.github.com>
2019-06-30 21:02:31 +0300

    Fix lint

--- src/PIL/TiffImagePlugin.py
+++ src/PIL/TiffImagePlugin.py
@@ -1542,7 +1542,7 @@ def _save(im, fp, filename):
 
         # optional types for non core tags
         types = {}
-        # SAMPLEFORMAT is determined by the image format and should not be copied 
+        # SAMPLEFORMAT is determined by the image format and should not be copied
         # from legacy_ifd.
         # STRIPOFFSETS and STRIPBYTECOUNTS are added by the library
         # based on the data in the strip.
