python-pillow/Pillow

commit 7a4af2b7671b14163f61c7f88889fad66e50d77b

fix unaligned accesses by using memcpy()

src/libImaging/Convert.c
@@ -229,42 +229,46 @@ static void
 rgb2i(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    INT32* out = (INT32*) out_;
-    for (x = 0; x < xsize; x++, in += 4)
-        *out++ = L24(in) >> 16;
+    for (x = 0; x < xsize; x++, in += 4, out_ += 4) {
+        INT32 v = L24(in) >> 16;
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void
 rgb2f(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    FLOAT32* out = (FLOAT32*) out_;
-    for (x = 0; x < xsize; x++, in += 4)
-        *out++ = (float) L(in) / 1000.0F;
+    for (x = 0; x < xsize; x++, in += 4, out_ += 4) {
+        FLOAT32 v = (float) L(in) / 1000.0F;
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void
 rgb2bgr15(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    UINT16* out = (UINT16*) out_;
-    for (x = 0; x < xsize; x++, in += 4)
-        *out++ =
+    for (x = 0; x < xsize; x++, in += 4, out_ += 2) {
+        UINT16 v =
             ((((UINT16)in[0])<<7)&0x7c00) +
             ((((UINT16)in[1])<<2)&0x03e0) +
             ((((UINT16)in[2])>>3)&0x001f);
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void
 rgb2bgr16(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    UINT16* out = (UINT16*) out_;
-    for (x = 0; x < xsize; x++, in += 4)
-        *out++ =
+    for (x = 0; x < xsize; x++, in += 4, out_ += 2) {
+        UINT16 v =
             ((((UINT16)in[0])<<8)&0xf800) +
             ((((UINT16)in[1])<<3)&0x07e0) +
             ((((UINT16)in[2])>>3)&0x001f);
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void
@@ -490,12 +494,13 @@ rgbT2rgba(UINT8* out, int xsize, int r, int g, int b)
     UINT32 repl = trns & 0x00ffffff;
 #endif
 
-    UINT32* tmp = (UINT32 *)out;
     int i;
 
-    for (i=0; i < xsize; i++ ,tmp++) {
-        if (tmp[0]==trns) {
-            tmp[0]=repl;
+    for (i=0; i < xsize; i++ ,out += sizeof(trns)) {
+        UINT32 v;
+        memcpy(&v, out, sizeof(v));
+        if (v==trns) {
+            memcpy(out, &repl, sizeof(repl));
         }
     }
 }
@@ -565,32 +570,35 @@ static void
 bit2i(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    INT32* out = (INT32*) out_;
-    for (x = 0; x < xsize; x++)
-        *out++ = (*in++ != 0) ? 255 : 0;
+    for (x = 0; x < xsize; x++, out_ += 4) {
+        INT32 v = (*in++ != 0) ? 255 : 0;
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void
 l2i(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    INT32* out = (INT32*) out_;
-    for (x = 0; x < xsize; x++)
-        *out++ = (INT32) *in++;
+    for (x = 0; x < xsize; x++, out_ += 4) {
+        INT32 v = *in++;
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void
 i2l(UINT8* out, const UINT8* in_, int xsize)
 {
     int x;
-    INT32* in = (INT32*) in_;
-    for (x = 0; x < xsize; x++, in++, out++) {
-        if (*in <= 0)
+    for (x = 0; x < xsize; x++, out++, in_ += 4) {
+        INT32 v;
+        memcpy(&v, in_, sizeof(v));
+        if (v <= 0)
             *out = 0;
-        else if (*in >= 255)
+        else if (v >= 255)
             *out = 255;
         else
-            *out = (UINT8) *in;
+            *out = (UINT8) v;
     }
 }
 
@@ -598,10 +606,13 @@ static void
 i2f(UINT8* out_, const UINT8* in_, int xsize)
 {
     int x;
-    INT32* in = (INT32*) in_;
-    FLOAT32* out = (FLOAT32*) out_;
-    for (x = 0; x < xsize; x++)
-        *out++ = (FLOAT32) *in++;
+    for (x = 0; x < xsize; x++, in_ += 4, out_ += 4) {
+        INT32 i;
+        FLOAT32 f;
+        memcpy(&i, in_, sizeof(i));
+        f = i;
+        memcpy(out_, &f, sizeof(f));
+    }
 }
 
 static void
@@ -628,32 +639,35 @@ static void
 bit2f(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    FLOAT32* out = (FLOAT32*) out_;
-    for (x = 0; x < xsize; x++)
-        *out++ = (*in++ != 0) ? 255.0F : 0.0F;
+    for (x = 0; x < xsize; x++, out_ += 4) {
+        FLOAT32 f = (*in++ != 0) ? 255.0F : 0.0F;
+        memcpy(out_, &f, sizeof(f));
+    }
 }
 
 static void
 l2f(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    FLOAT32* out = (FLOAT32*) out_;
-    for (x = 0; x < xsize; x++)
-        *out++ = (FLOAT32) *in++;
+    for (x = 0; x < xsize; x++, out_ += 4) {
+        FLOAT32 f = (FLOAT32) *in++;
+        memcpy(out_, &f, sizeof(f));
+    }
 }
 
 static void
 f2l(UINT8* out, const UINT8* in_, int xsize)
 {
     int x;
-    FLOAT32* in = (FLOAT32*) in_;
-    for (x = 0; x < xsize; x++, in++, out++) {
-        if (*in <= 0.0)
+    for (x = 0; x < xsize; x++, out++, in_ += 4) {
+        FLOAT32 v;
+        memcpy(&v, in_, sizeof(v));
+        if (v <= 0.0)
             *out = 0;
-        else if (*in >= 255.0)
+        else if (v >= 255.0)
             *out = 255;
         else
-            *out = (UINT8) *in;
+            *out = (UINT8) v;
     }
 }
 
@@ -661,10 +675,13 @@ static void
 f2i(UINT8* out_, const UINT8* in_, int xsize)
 {
     int x;
-    FLOAT32* in = (FLOAT32*) in_;
-    INT32* out = (INT32*) out_;
-    for (x = 0; x < xsize; x++)
-        *out++ = (INT32) *in++;
+    for (x = 0; x < xsize; x++, in_ += 4, out_ += 4) {
+        FLOAT32 f;
+        INT32 i;
+        memcpy(&f, in_, sizeof(f));
+        i = f;
+        memcpy(out_, &i, sizeof(i));
+    }
 }
 
 /* ----------------- */
@@ -723,9 +740,10 @@ static void
 I_I16L(UINT8* out, const UINT8* in_, int xsize)
 {
     int x, v;
-    INT32* in = (INT32*) in_;
-    for (x = 0; x < xsize; x++, in++) {
-        v = CLIP16(*in);
+    for (x = 0; x < xsize; x++, in_ += 4) {
+        INT32 i;
+        memcpy(&i, in_, sizeof(i));
+        v = CLIP16(i);
         *out++ = (UINT8) v;
         *out++ = (UINT8) (v >> 8);
     }
@@ -735,9 +753,10 @@ static void
 I_I16B(UINT8* out, const UINT8* in_, int xsize)
 {
     int x, v;
-    INT32* in = (INT32*) in_;
-    for (x = 0; x < xsize; x++, in++) {
-        v = CLIP16(*in);
+    for (x = 0; x < xsize; x++, in_ += 4) {
+        INT32 i;
+        memcpy(&i, in_, sizeof(i));
+        v = CLIP16(i);
         *out++ = (UINT8) (v >> 8);
         *out++ = (UINT8) v;
     }
@@ -748,9 +767,10 @@ static void
 I16L_I(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    INT32* out = (INT32*) out_;
-    for (x = 0; x < xsize; x++, in += 2)
-        *out++ = in[0] + ((int) in[1] << 8);
+    for (x = 0; x < xsize; x++, in += 2, out_ += 4) {
+        INT32 v = in[0] + ((int) in[1] << 8);
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 
@@ -758,18 +778,20 @@ static void
 I16B_I(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    INT32* out = (INT32*) out_;
-    for (x = 0; x < xsize; x++, in += 2)
-        *out++ = in[1] + ((int) in[0] << 8);
+    for (x = 0; x < xsize; x++, in += 2, out_ += 4) {
+        INT32 v = in[1] + ((int) in[0] << 8);
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void
 I16L_F(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    FLOAT32* out = (FLOAT32*) out_;
-    for (x = 0; x < xsize; x++, in += 2)
-        *out++ = (FLOAT32) (in[0] + ((int) in[1] << 8));
+    for (x = 0; x < xsize; x++, in += 2, out_ += 4) {
+        FLOAT32 v = in[0] + ((int) in[1] << 8);
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 
@@ -777,9 +799,10 @@ static void
 I16B_F(UINT8* out_, const UINT8* in, int xsize)
 {
     int x;
-    FLOAT32* out = (FLOAT32*) out_;
-    for (x = 0; x < xsize; x++, in += 2)
-        *out++ = (FLOAT32) (in[1] + ((int) in[0] << 8));
+    for (x = 0; x < xsize; x++, in += 2, out_ += 4) {
+        FLOAT32 v = in[1] + ((int) in[0] << 8);
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void
@@ -1018,9 +1041,10 @@ static void
 p2i(UINT8* out_, const UINT8* in, int xsize, const UINT8* palette)
 {
     int x;
-    INT32* out = (INT32*) out_;
-    for (x = 0; x < xsize; x++)
-        *out++ = L(&palette[in[x]*4]) / 1000;
+    for (x = 0; x < xsize; x++, in += 2, out_ += 4) {
+        INT32 v = L(&palette[in[x]*4]) / 1000;
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void
@@ -1036,9 +1060,10 @@ static void
 p2f(UINT8* out_, const UINT8* in, int xsize, const UINT8* palette)
 {
     int x;
-    FLOAT32* out = (FLOAT32*) out_;
-    for (x = 0; x < xsize; x++)
-        *out++ = (float) L(&palette[in[x]*4]) / 1000.0F;
+    for (x = 0; x < xsize; x++, in += 2, out_ += 4) {
+        FLOAT32 v = L(&palette[in[x]*4]) / 1000.0F;
+        memcpy(out_, &v, sizeof(v));
+    }
 }
 
 static void

src/_imaging.c
@@ -382,7 +382,8 @@ getlist(PyObject* arg, Py_ssize_t* length, const char* wrong_length, int type)
     Py_ssize_t i, n;
     int itemp;
     double dtemp;
-    void* list;
+    FLOAT32 ftemp;
+    UINT8* list;
     PyObject* seq;
     PyObject* op;
 
@@ -416,19 +417,19 @@ getlist(PyObject* arg, Py_ssize_t* length, const char* wrong_length, int type)
         switch (type) {
         case TYPE_UINT8:
             itemp = PyInt_AsLong(op);
-            ((UINT8*)list)[i] = CLIP8(itemp);
+            list[i] = CLIP8(itemp);
             break;
         case TYPE_INT32:
             itemp = PyInt_AsLong(op);
-            ((INT32*)list)[i] = itemp;
+            memcpy(list + i * sizeof(INT32), &itemp, sizeof(itemp));
             break;
         case TYPE_FLOAT32:
-            dtemp = PyFloat_AsDouble(op);
-            ((FLOAT32*)list)[i] = (FLOAT32) dtemp;
+            ftemp = (FLOAT32)PyFloat_AsDouble(op);
+            memcpy(list + i * sizeof(ftemp), &ftemp, sizeof(ftemp));
             break;
         case TYPE_DOUBLE:
             dtemp = PyFloat_AsDouble(op);
-            ((double*)list)[i] = (double) dtemp;
+            memcpy(list + i * sizeof(dtemp), &dtemp, sizeof(dtemp));
             break;
         }
     }
@@ -532,6 +533,8 @@ getink(PyObject* color, Imaging im, char* ink)
        to return it into a 32 bit C long
     */
     PY_LONG_LONG r = 0;
+    FLOAT32 ftmp;
+    INT32 itmp;
 
     /* fill ink buffer (four bytes) with something that can
        be cast to either UINT8 or INT32 */
@@ -597,14 +600,16 @@ getink(PyObject* color, Imaging im, char* ink)
         /* signed integer */
         if (rIsInt != 1)
             return NULL;
-        *(INT32*) ink = r;
+        itmp = r;
+        memcpy(ink, &itmp, sizeof(itmp));
         return ink;
     case IMAGING_TYPE_FLOAT32:
         /* floating point */
         f = PyFloat_AsDouble(color);
         if (f == -1.0 && PyErr_Occurred())
             return NULL;
-        *(FLOAT32*) ink = (FLOAT32) f;
+        ftmp = f;
+        memcpy(ink, &ftmp, sizeof(ftmp));
         return ink;
     case IMAGING_TYPE_SPECIAL:
         if (strncmp(im->mode, "I;16", 4) == 0) {
@@ -799,15 +804,19 @@ _prepare_lut_table(PyObject* table, Py_ssize_t table_size)
     }
 
     for (i = 0; i < table_size; i++) {
+        FLOAT16 htmp;
+        double dtmp;
         switch (data_type) {
             case TYPE_FLOAT16:
-                item = float16tofloat32(((FLOAT16*) table_data)[i]);
+                memcpy(&htmp, ((char*) table_data) + i * sizeof(htmp), sizeof(htmp));
+                item = float16tofloat32(htmp);
                 break;
             case TYPE_FLOAT32:
-                item = ((FLOAT32*) table_data)[i];
+                memcpy(&item, ((char*) table_data) + i * sizeof(FLOAT32), sizeof(FLOAT32));
                 break;
             case TYPE_DOUBLE:
-                item = ((double*) table_data)[i];
+                memcpy(&dtmp, ((char*) table_data) + i * sizeof(dtmp), sizeof(dtmp));
+                item = (FLOAT32) dtmp;
                 break;
         }
         /* Max value for INT16 */

src/libImaging/Access.c
@@ -95,8 +95,8 @@ get_pixel_16L(Imaging im, int x, int y, void* color)
 {
     UINT8* in = (UINT8*) &im->image[y][x+x];
 #ifdef WORDS_BIGENDIAN
-    UINT16* out = color;
-    out[0] = in[0] + (in[1]<<8);
+    UINT16 out = in[0] + (in[1]<<8);
+    memcpy(color, &out, sizeof(out));
 #else
     memcpy(color, in, sizeof(UINT16));
 #endif
@@ -109,8 +109,8 @@ get_pixel_16B(Imaging im, int x, int y, void* color)
 #ifdef WORDS_BIGENDIAN
     memcpy(color, in, sizeof(UINT16));
 #else
-    UINT16* out = color;
-    out[0] = in[1] + (in[0]<<8);
+    UINT16 out = in[1] + (in[0]<<8);
+    memcpy(color, &out, sizeof(out));
 #endif
 }
 
@@ -125,8 +125,8 @@ get_pixel_32L(Imaging im, int x, int y, void* color)
 {
     UINT8* in = (UINT8*) &im->image[y][x*4];
 #ifdef WORDS_BIGENDIAN
-    INT32* out = color;
-    out[0] = in[0] + (in[1]<<8) + (in[2]<<16) + (in[3]<<24);
+    INT32 out = in[0] + (in[1]<<8) + (in[2]<<16) + (in[3]<<24);
+    memcpy(color, &out, sizeof(out));
 #else
     memcpy(color, in, sizeof(INT32));
 #endif
@@ -139,8 +139,8 @@ get_pixel_32B(Imaging im, int x, int y, void* color)
 #ifdef WORDS_BIGENDIAN
     memcpy(color, in, sizeof(INT32));
 #else
-    INT32* out = color;
-    out[0] = in[3] + (in[2]<<8) + (in[1]<<16) + (in[0]<<24);
+    INT32 out = in[3] + (in[2]<<8) + (in[1]<<16) + (in[0]<<24);
+    memcpy(color, &out, sizeof(out));
 #endif
 }
 

src/libImaging/Bands.c
@@ -50,7 +50,8 @@ ImagingGetBand(Imaging imIn, int band)
         UINT8* out = imOut->image8[y];
         x = 0;
         for (; x < imIn->xsize - 3; x += 4) {
-            *((UINT32*) (out + x)) = MAKE_UINT32(in[0], in[4], in[8], in[12]);
+            UINT32 v = MAKE_UINT32(in[0], in[4], in[8], in[12]);
+            memcpy(out + x, &v, sizeof(v));
             in += 16;
         }
         for (; x < imIn->xsize; x++) {
@@ -98,8 +99,10 @@ ImagingSplit(Imaging imIn, Imaging bands[4])
             UINT8* out1 = bands[1]->image8[y];
             x = 0;
             for (; x < imIn->xsize - 3; x += 4) {
-                *((UINT32*) (out0 + x)) = MAKE_UINT32(in[0], in[4], in[8], in[12]);
-                *((UINT32*) (out1 + x)) = MAKE_UINT32(in[0+3], in[4+3], in[8+3], in[12+3]);
+                UINT32 v = MAKE_UINT32(in[0], in[4], in[8], in[12]);
+                memcpy(out0 + x, &v, sizeof(v));
+                v = MAKE_UINT32(in[0+3], in[4+3], in[8+3], in[12+3]);
+                memcpy(out1 + x, &v, sizeof(v));
                 in += 16;
             }
             for (; x < imIn->xsize; x++) {
@@ -116,9 +119,12 @@ ImagingSplit(Imaging imIn, Imaging bands[4])
             UINT8* out2 = bands[2]->image8[y];
             x = 0;
             for (; x < imIn->xsize - 3; x += 4) {
-                *((UINT32*) (out0 + x)) = MAKE_UINT32(in[0], in[4], in[8], in[12]);
-                *((UINT32*) (out1 + x)) = MAKE_UINT32(in[0+1], in[4+1], in[8+1], in[12+1]);
-                *((UINT32*) (out2 + x)) = MAKE_UINT32(in[0+2], in[4+2], in[8+2], in[12+2]);
+                UINT32 v = MAKE_UINT32(in[0], in[4], in[8], in[12]);
+                memcpy(out0 + x, &v, sizeof(v));
+                v = MAKE_UINT32(in[0+1], in[4+1], in[8+1], in[12+1]);
+                memcpy(out1 + x, &v, sizeof(v));
+                v = MAKE_UINT32(in[0+2], in[4+2], in[8+2], in[12+2]);
+                memcpy(out2 + x, &v, sizeof(v));
                 in += 16;
             }
             for (; x < imIn->xsize; x++) {
@@ -137,10 +143,14 @@ ImagingSplit(Imaging imIn, Imaging bands[4])
             UINT8* out3 = bands[3]->image8[y];
             x = 0;
             for (; x < imIn->xsize - 3; x += 4) {
-                *((UINT32*) (out0 + x)) = MAKE_UINT32(in[0], in[4], in[8], in[12]);
-                *((UINT32*) (out1 + x)) = MAKE_UINT32(in[0+1], in[4+1], in[8+1], in[12+1]);
-                *((UINT32*) (out2 + x)) = MAKE_UINT32(in[0+2], in[4+2], in[8+2], in[12+2]);
-                *((UINT32*) (out3 + x)) = MAKE_UINT32(in[0+3], in[4+3], in[8+3], in[12+3]);
+                UINT32 v = MAKE_UINT32(in[0], in[4], in[8], in[12]);
+                memcpy(out0 + x, &v, sizeof(v));
+                v = MAKE_UINT32(in[0+1], in[4+1], in[8+1], in[12+1]);
+                memcpy(out1 + x, &v, sizeof(v));
+                v = MAKE_UINT32(in[0+2], in[4+2], in[8+2], in[12+2]);
+                memcpy(out2 + x, &v, sizeof(v));
+                v = MAKE_UINT32(in[0+3], in[4+3], in[8+3], in[12+3]);
+                memcpy(out3 + x, &v, sizeof(v));
                 in += 16;
             }
             for (; x < imIn->xsize; x++) {

src/libImaging/ColorLUT.c
@@ -105,7 +105,7 @@ ImagingColorLUT3D_linear(Imaging imOut, Imaging imIn, int table_channels,
     ImagingSectionEnter(&cookie);
     for (y = 0; y < imOut->ysize; y++) {
         UINT8* rowIn = (UINT8 *)imIn->image[y];
-        UINT32* rowOut = (UINT32 *)imOut->image[y];
+        char* rowOut = (char *)imOut->image[y];
         for (x = 0; x < imOut->xsize; x++) {
             UINT32 index1D = rowIn[x*4 + 0] * scale1D;
             UINT32 index2D = rowIn[x*4 + 1] * scale2D;
@@ -120,6 +120,7 @@ ImagingColorLUT3D_linear(Imaging imOut, Imaging imIn, int table_channels,
             INT16 leftleft[4], leftright[4], rightleft[4], rightright[4];
 
             if (table_channels == 3) {
+                UINT32 v;
                 interpolate3(leftleft, &table[idx + 0], &table[idx + 3], shift1D);
                 interpolate3(leftright, &table[idx + size1D*3],
                              &table[idx + size1D*3 + 3], shift1D);
@@ -133,12 +134,14 @@ ImagingColorLUT3D_linear(Imaging imOut, Imaging imIn, int table_channels,
 
                 interpolate3(result, left, right, shift3D);
 
-                rowOut[x] = MAKE_UINT32(
+                v = MAKE_UINT32(
                         clip8(result[0]), clip8(result[1]),
                         clip8(result[2]), rowIn[x*4 + 3]);
+                memcpy(rowOut + x * sizeof(v), &v, sizeof(v));
             }
 
             if (table_channels == 4) {
+                UINT32 v;
                 interpolate4(leftleft, &table[idx + 0], &table[idx + 4], shift1D);
                 interpolate4(leftright, &table[idx + size1D*4],
                              &table[idx + size1D*4 + 4], shift1D);
@@ -152,9 +155,10 @@ ImagingColorLUT3D_linear(Imaging imOut, Imaging imIn, int table_channels,
 
                 interpolate4(result, left, right, shift3D);
 
-                rowOut[x] = MAKE_UINT32(
+                v = MAKE_UINT32(
                         clip8(result[0]), clip8(result[1]),
                         clip8(result[2]), clip8(result[3]));
+                memcpy(rowOut + x * sizeof(v), &v, sizeof(v));
             }
         }
     }

src/libImaging/Filter.c
@@ -122,26 +122,29 @@ ImagingFilter3x3(Imaging imOut, Imaging im, const float* kernel,
             UINT8* in_1 = (UINT8*) im->image[y-1];
             UINT8* in0 = (UINT8*) im->image[y];
             UINT8* in1 = (UINT8*) im->image[y+1];
-            UINT32* out = (UINT32*) imOut->image[y];
+            UINT8* out = (UINT8*) imOut->image[y];
 
             memcpy(out, in0, sizeof(UINT32));
             if (im->bands == 2) {
                 for (x = 1; x < im->xsize-1; x++) {
                     float ss0 = offset;
                     float ss3 = offset;
+                    UINT32 v;
                     ss0 += KERNEL1x3(in1, x*4+0, &kernel[0], 4);
                     ss3 += KERNEL1x3(in1, x*4+3, &kernel[0], 4);
                     ss0 += KERNEL1x3(in0, x*4+0, &kernel[3], 4);
                     ss3 += KERNEL1x3(in0, x*4+3, &kernel[3], 4);
                     ss0 += KERNEL1x3(in_1, x*4+0, &kernel[6], 4);
                     ss3 += KERNEL1x3(in_1, x*4+3, &kernel[6], 4);
-                    out[x] = MAKE_UINT32(clip8(ss0), 0, 0, clip8(ss3));
+                    v = MAKE_UINT32(clip8(ss0), 0, 0, clip8(ss3));
+                    memcpy(out + x * sizeof(v), &v, sizeof(v));
                 }
             } else if (im->bands == 3) {
                 for (x = 1; x < im->xsize-1; x++) {
                     float ss0 = offset;
                     float ss1 = offset;
                     float ss2 = offset;
+                    UINT32 v;
                     ss0 += KERNEL1x3(in1, x*4+0, &kernel[0], 4);
                     ss1 += KERNEL1x3(in1, x*4+1, &kernel[0], 4);
                     ss2 += KERNEL1x3(in1, x*4+2, &kernel[0], 4);
@@ -151,8 +154,9 @@ ImagingFilter3x3(Imaging imOut, Imaging im, const float* kernel,
                     ss0 += KERNEL1x3(in_1, x*4+0, &kernel[6], 4);
                     ss1 += KERNEL1x3(in_1, x*4+1, &kernel[6], 4);
                     ss2 += KERNEL1x3(in_1, x*4+2, &kernel[6], 4);
-                    out[x] = MAKE_UINT32(
+                    v = MAKE_UINT32(
                         clip8(ss0), clip8(ss1), clip8(ss2), 0);
+                    memcpy(out + x * sizeof(v), &v, sizeof(v));
                 }
             } else if (im->bands == 4) {
                 for (x = 1; x < im->xsize-1; x++) {
@@ -160,6 +164,7 @@ ImagingFilter3x3(Imaging imOut, Imaging im, const float* kernel,
                     float ss1 = offset;
                     float ss2 = offset;
                     float ss3 = offset;
+                    UINT32 v;
                     ss0 += KERNEL1x3(in1, x*4+0, &kernel[0], 4);
                     ss1 += KERNEL1x3(in1, x*4+1, &kernel[0], 4);
                     ss2 += KERNEL1x3(in1, x*4+2, &kernel[0], 4);
@@ -172,11 +177,12 @@ ImagingFilter3x3(Imaging imOut, Imaging im, const float* kernel,
                     ss1 += KERNEL1x3(in_1, x*4+1, &kernel[6], 4);
                     ss2 += KERNEL1x3(in_1, x*4+2, &kernel[6], 4);
                     ss3 += KERNEL1x3(in_1, x*4+3, &kernel[6], 4);
-                    out[x] = MAKE_UINT32(
+                    v = MAKE_UINT32(
                         clip8(ss0), clip8(ss1), clip8(ss2), clip8(ss3));
+                    memcpy(out + x * sizeof(v), &v, sizeof(v));
                 }
             }
-            out[x] = ((UINT32*) in0)[x];
+            memcpy(out + x * sizeof(UINT32), in0 + x * sizeof(UINT32), sizeof(UINT32));
         }
     }
     memcpy(imOut->image[y], im->image[y], im->linesize);
@@ -232,13 +238,14 @@ ImagingFilter5x5(Imaging imOut, Imaging im, const float* kernel,
             UINT8* in0 = (UINT8*) im->image[y];
             UINT8* in1 = (UINT8*) im->image[y+1];
             UINT8* in2 = (UINT8*) im->image[y+2];
-            UINT32* out = (UINT32*) imOut->image[y];
+            UINT8* out = (UINT8*) imOut->image[y];
 
             memcpy(out, in0, sizeof(UINT32) * 2);
             if (im->bands == 2) {
                 for (x = 2; x < im->xsize-2; x++) {
                     float ss0 = offset;
                     float ss3 = offset;
+                    UINT32 v;
                     ss0 += KERNEL1x5(in2, x*4+0, &kernel[0], 4);
                     ss3 += KERNEL1x5(in2, x*4+3, &kernel[0], 4);
                     ss0 += KERNEL1x5(in1, x*4+0, &kernel[5], 4);
@@ -249,13 +256,15 @@ ImagingFilter5x5(Imaging imOut, Imaging im, const float* kernel,
                     ss3 += KERNEL1x5(in_1, x*4+3, &kernel[15], 4);
                     ss0 += KERNEL1x5(in_2, x*4+0, &kernel[20], 4);
                     ss3 += KERNEL1x5(in_2, x*4+3, &kernel[20], 4);
-                    out[x] = MAKE_UINT32(clip8(ss0), 0, 0, clip8(ss3));
+                    v = MAKE_UINT32(clip8(ss0), 0, 0, clip8(ss3));
+                    memcpy(out + x * sizeof(v), &v, sizeof(v));
                 }
             } else if (im->bands == 3) {
                 for (x = 2; x < im->xsize-2; x++) {
                     float ss0 = offset;
                     float ss1 = offset;
                     float ss2 = offset;
+                    UINT32 v;
                     ss0 += KERNEL1x5(in2, x*4+0, &kernel[0], 4);
                     ss1 += KERNEL1x5(in2, x*4+1, &kernel[0], 4);
                     ss2 += KERNEL1x5(in2, x*4+2, &kernel[0], 4);
@@ -271,8 +280,9 @@ ImagingFilter5x5(Imaging imOut, Imaging im, const float* kernel,
                     ss0 += KERNEL1x5(in_2, x*4+0, &kernel[20], 4);
                     ss1 += KERNEL1x5(in_2, x*4+1, &kernel[20], 4);
                     ss2 += KERNEL1x5(in_2, x*4+2, &kernel[20], 4);
-                    out[x] = MAKE_UINT32(
+                    v = MAKE_UINT32(
                         clip8(ss0), clip8(ss1), clip8(ss2), 0);
+                    memcpy(out + x * sizeof(v), &v, sizeof(v));
                 }
             } else if (im->bands == 4) {
                 for (x = 2; x < im->xsize-2; x++) {
@@ -280,6 +290,7 @@ ImagingFilter5x5(Imaging imOut, Imaging im, const float* kernel,
                     float ss1 = offset;
                     float ss2 = offset;
                     float ss3 = offset;
+                    UINT32 v;
                     ss0 += KERNEL1x5(in2, x*4+0, &kernel[0], 4);
                     ss1 += KERNEL1x5(in2, x*4+1, &kernel[0], 4);
                     ss2 += KERNEL1x5(in2, x*4+2, &kernel[0], 4);
@@ -300,12 +311,12 @@ ImagingFilter5x5(Imaging imOut, Imaging im, const float* kernel,
                     ss1 += KERNEL1x5(in_2, x*4+1, &kernel[20], 4);
                     ss2 += KERNEL1x5(in_2, x*4+2, &kernel[20], 4);
                     ss3 += KERNEL1x5(in_2, x*4+3, &kernel[20], 4);
-                    out[x] = MAKE_UINT32(
+                    v = MAKE_UINT32(
                         clip8(ss0), clip8(ss1), clip8(ss2), clip8(ss3));
+                    memcpy(out + x * sizeof(v), &v, sizeof(v));
                 }
             }
-            out[x] = ((UINT32*) in0)[x];
-            out[x+1] = ((UINT32*) in0)[x+1];
+            memcpy(out + x * sizeof(UINT32), in0 + x * sizeof(UINT32), sizeof(UINT32) * 2);
         }
     }
     memcpy(imOut->image[y], im->image[y], im->linesize);

src/libImaging/Geometry.c
@@ -428,7 +428,7 @@ nearest_filter16(void* out, Imaging im, double xin, double yin)
     int y = COORD(yin);
     if (x < 0 || x >= im->xsize || y < 0 || y >= im->ysize)
         return 0;
-    ((INT16*)out)[0] = ((INT16*)(im->image8[y]))[x];
+    memcpy(out, im->image8[y] + x * sizeof(INT16), sizeof(INT16));
     return 1;
 }
 
@@ -489,18 +489,22 @@ bilinear_filter8(void* out, Imaging im, double xin, double yin)
 static int
 bilinear_filter32I(void* out, Imaging im, double xin, double yin)
 {
+    INT32 k;
     BILINEAR_HEAD(INT32);
     BILINEAR_BODY(INT32, im->image32, 1, 0);
-    ((INT32*)out)[0] = (INT32) v1;
+    k = v1;
+    memcpy(out, &k, sizeof(k));
     return 1;
 }
 
 static int
 bilinear_filter32F(void* out, Imaging im, double xin, double yin)
 {
+    FLOAT32 k;
     BILINEAR_HEAD(FLOAT32);
     BILINEAR_BODY(FLOAT32, im->image32, 1, 0);
-    ((FLOAT32*)out)[0] = (FLOAT32) v1;
+    k = v1;
+    memcpy(out, &k, sizeof(k));
     return 1;
 }
 
@@ -601,18 +605,22 @@ bicubic_filter8(void* out, Imaging im, double xin, double yin)
 static int
 bicubic_filter32I(void* out, Imaging im, double xin, double yin)
 {
+    INT32 k;
     BICUBIC_HEAD(INT32);
     BICUBIC_BODY(INT32, im->image32, 1, 0);
-    ((INT32*)out)[0] = (INT32) v1;
+    k = v1;
+    memcpy(out, &k, sizeof(k));
     return 1;
 }
 
 static int
 bicubic_filter32F(void* out, Imaging im, double xin, double yin)
 {
+    FLOAT32 k;
     BICUBIC_HEAD(FLOAT32);
     BICUBIC_BODY(FLOAT32, im->image32, 1, 0);
-    ((FLOAT32*)out)[0] = (FLOAT32) v1;
+    k = v1;
+    memcpy(out, &k, sizeof(k));
     return 1;
 }
 

src/libImaging/GetBBox.c
@@ -147,7 +147,7 @@ ImagingGetExtrema(Imaging im, void *extrema)
             }
         }
         memcpy(extrema, &imin, sizeof(imin));
-        ((INT32*) extrema)[1] = imax;
+        memcpy(((char*)extrema) + sizeof(imin), &imax, sizeof(imax));
         break;
     case IMAGING_TYPE_FLOAT32:
         fmin = fmax = ((FLOAT32*) im->image32[0])[0];
@@ -161,22 +161,26 @@ ImagingGetExtrema(Imaging im, void *extrema)
             }
         }
         memcpy(extrema, &fmin, sizeof(fmin));
-        ((FLOAT32*) extrema)[1] = fmax;
+        memcpy(((char*)extrema) + sizeof(fmin), &fmax, sizeof(fmax));
         break;
     case IMAGING_TYPE_SPECIAL:
       if (strcmp(im->mode, "I;16") == 0) {
-          imin = imax = ((UINT16*) im->image8[0])[0];
+          UINT16 v;
+          memcpy(&v, *im->image8, sizeof(v));
+          imin = imax = v;
           for (y = 0; y < im->ysize; y++) {
-              UINT16* in = (UINT16 *) im->image[y];
               for (x = 0; x < im->xsize; x++) {
-                  if (imin > in[x])
-                      imin = in[x];
-                  else if (imax < in[x])
-                      imax = in[x];
+                  memcpy(&v, im->image[y] + x * sizeof(v), sizeof(v));
+                  if (imin > v)
+                      imin = v;
+                  else if (imax < v)
+                      imax = v;
               }
           }
-          ((UINT16*) extrema)[0] = (UINT16) imin;
-          ((UINT16*) extrema)[1] = (UINT16) imax;
+          v = (UINT16) imin;
+          memcpy(extrema, &v, sizeof(v));
+          v = (UINT16) imax;
+          memcpy(((char*)extrema) + sizeof(v), &v, sizeof(v));
 	  break;
       }
       /* FALL THROUGH */

commit 0e0afd4acfaf130f21e3abfce1da00666bff5809

fix bad loop increments in p2i() and p2f()
Fixes: 7a4af2b7671b14163f61c7f88889fad66e50d77b

src/libImaging/Convert.c
@@ -1041,7 +1041,7 @@ static void
 p2i(UINT8* out_, const UINT8* in, int xsize, const UINT8* palette)
 {
     int x;
-    for (x = 0; x < xsize; x++, in += 2, out_ += 4) {
+    for (x = 0; x < xsize; x++, out_ += 4) {
         INT32 v = L(&palette[in[x]*4]) / 1000;
         memcpy(out_, &v, sizeof(v));
     }
@@ -1060,7 +1060,7 @@ static void
 p2f(UINT8* out_, const UINT8* in, int xsize, const UINT8* palette)
 {
     int x;
-    for (x = 0; x < xsize; x++, in += 2, out_ += 4) {
+    for (x = 0; x < xsize; x++, out_ += 4) {
         FLOAT32 v = L(&palette[in[x]*4]) / 1000.0F;
         memcpy(out_, &v, sizeof(v));
     }