ce7c8e8bbc43a9d83d91929714e53d4a7af2185a
Jon Dufresne <jon.dufresne@gmail.com>
2019-07-02 17:52:30 -0700

    Remove check-manifest from .travis/script.sh
    check-manifest already exists as part of the lint tox target "lint"
    which is run by Travis CI. There is no need to re-run the same check as
    part of each test run.

--- .travis/script.sh
+++ .travis/script.sh
@@ -9,7 +9,5 @@ make install-coverage
 python selftest.py
 python -m pytest -v -x --cov PIL --cov-report term Tests
 
-pushd /tmp/check-manifest && check-manifest --ignore ".coveragerc,.editorconfig,*.yml,*.yaml,tox.ini" && popd
-
 # Docs
 if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ]; then make doccheck; fi
