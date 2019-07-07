388803006c01f52c09ec527028d7d84f08950ecd
Jon Dufresne <jon.dufresne@gmail.com>
2019-07-06 17:25:14 -0700

    Remove unused docs/_templates directory
    The only file, sidebarhelp.html, applies to the sphinx-better-theme
    which is unused and was removed in
    02f3685b2d0f96cbe7028d45c0564efb0c5349ad.

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,6 @@ import PIL
 # ones.
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.intersphinx"]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
