9c7d3a0e4a0537dfe54b7a585da3d8246c10a8cc
paddywwoof <patrick@eldwick.org.uk>
2019-07-30 18:06:43 +0100

    fix unpack to c_ssize_t on 32 bit systems with corrupt EXIF data

--- src/PIL/TiffImagePlugin.py
+++ src/PIL/TiffImagePlugin.py
@@ -771,7 +771,12 @@ class ImageFileDirectory_v2(MutableMapping):
                             "Tag Location: %s - Data Location: %s" % (here, offset),
                             end=" ",
                         )
-                    fp.seek(offset)
+                    try:
+                        fp.seek(offset)
+                    except OverflowError as e:
+                        if DEBUG:
+                            print("- offset value wrong. Possibly corrupt EXIF data. ", e)
+                        continue  # ignore corrupt EXIF data
                     data = ImageFile._safe_read(fp, size)
                     fp.seek(here)
                 else:
