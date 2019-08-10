3fbea030264e3f780c26abfee553f53e6cd8434d
Andrew Murray <radarhere@users.noreply.github.com>
2019-08-11 07:23:08 +1000

    Added fallback for finding ld on MinGW Cygwin

--- winbuild/appveyor_install_msys2_deps.sh
+++ winbuild/appveyor_install_msys2_deps.sh
@@ -3,12 +3,16 @@
 mkdir /var/cache/pacman/pkg
 pacman -S --noconfirm mingw32/mingw-w64-i686-python3-pip \
 	   mingw32/mingw-w64-i686-python3-setuptools \
+	   mingw32/mingw-w64-i686-python3-pytest \
+	   mingw32/mingw-w64-i686-python3-pytest-cov \
 	   mingw32/mingw-w64-i686-python2-pip \
 	   mingw32/mingw-w64-i686-python2-setuptools \
+	   mingw32/mingw-w64-i686-python2-pytest \
+	   mingw32/mingw-w64-i686-python2-pytest-cov \
 	   mingw-w64-i686-libjpeg-turbo \
 	   mingw-w64-i686-libimagequant
 
 C:/msys64/mingw32/bin/python3 -m pip install --upgrade pip
 
-/mingw32/bin/pip install pytest pytest-cov olefile
-/mingw32/bin/pip3 install pytest pytest-cov olefile
+/mingw32/bin/pip install olefile
+/mingw32/bin/pip3 install olefile
