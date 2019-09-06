76e5bd0f0f1c74cb00e02a2b9ff78607e4c49ca2
Andrew Murray <radarhere@users.noreply.github.com>
2019-09-06 20:07:23 +1000

    Added brackets

--- src/libImaging/Convert.c
+++ src/libImaging/Convert.c
@@ -698,12 +698,13 @@ i2hsv(UINT8* out, const UINT8* in_, int xsize)
     for (x = 0; x < xsize; x++, in++, out+=4) {
         out[0] = 0;
         out[1] = 0;
-        if (*in <= 0)
+        if (*in <= 0) {
             out[2] = 0;
-        else if (*in >= 255)
+        } else if (*in >= 255) {
             out[2] = 255;
-        else
+        } else {
             out[2] = (UINT8) *in;
+        }
         out[3] = 255;
     }
 }
