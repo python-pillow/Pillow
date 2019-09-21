5b04a735990cece6a7c1b76b0c88ec8fdb8deb0e
nulano <nulano@nulano.eu>
2019-09-21 15:25:57 +0200

    use nightly pypy3
    (cherry picked from commit 2d2076f2dc80a732575b1ed742cc706478804d2a)

--- winbuild/appveyor_install_pypy3.cmd
+++ winbuild/appveyor_install_pypy3.cmd
@@ -1,3 +1,3 @@
-curl -fsSL -o pypy3.zip https://bitbucket.org/pypy/pypy/downloads/pypy3.6-v7.1.1-win32.zip
+curl -fsSL -o pypy3.zip http://buildbot.pypy.org/nightly/py3.6/pypy-c-jit-97588-7392d01b93d0-win32.zip
 7z x pypy3.zip -oc:\
-c:\Python37\Scripts\virtualenv.exe -p c:\pypy3.6-v7.1.1-win32\pypy3.exe c:\vp\pypy3
+c:\Python37\Scripts\virtualenv.exe -p c:\pypy-c-jit-97588-7392d01b93d0-win32\pypy3.exe c:\vp\pypy3
