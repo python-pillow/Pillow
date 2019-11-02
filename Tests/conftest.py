5d10f8dff2b93914fa0de2e4aff1de1c412cb984
Christoph Gohlke <cjgohlke@gmail.com>
2019-11-02 15:12:52 -0700

    Use features.pilinfo to report details about Pillow

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,40 +1,11 @@
 def pytest_report_header(config):
-    import os
-
-    report = []
-
-    def append(*args):
-        report.append(" ".join(args))
+    import io
 
     try:
-        from PIL import Image, features
+        from PIL import features
 
-        append("-" * 68)
-        append("Pillow", Image.__version__)
-        append("-" * 68)
-        append("Python modules loaded from", os.path.dirname(Image.__file__))
-        append("Binary modules loaded from", os.path.dirname(Image.core.__file__))
-        append("-" * 68)
-        for name, feature in [
-            ("pil", "PIL CORE"),
-            ("tkinter", "TKINTER"),
-            ("freetype2", "FREETYPE2"),
-            ("littlecms2", "LITTLECMS2"),
-            ("webp", "WEBP"),
-            ("transp_webp", "WEBP Transparency"),
-            ("webp_mux", "WEBPMUX"),
-            ("webp_anim", "WEBP Animation"),
-            ("jpg", "JPEG"),
-            ("jpg_2000", "OPENJPEG (JPEG2000)"),
-            ("zlib", "ZLIB (PNG/ZIP)"),
-            ("libtiff", "LIBTIFF"),
-            ("raqm", "RAQM (Bidirectional Text)"),
-        ]:
-            if features.check(name):
-                append("---", feature, "support ok")
-            else:
-                append("***", feature, "support not installed")
-        append("-" * 68)
+        with io.StringIO() as out:
+            features.pilinfo(out=out, supported_formats=False)
+            return out.getvalue()
     except Exception as e:
         return "pytest_report_header failed: %s" % str(e)
-    return "\n".join(report)
