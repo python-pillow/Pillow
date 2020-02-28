b934b50867f51b7186119b22575d15fa45cdc7dc
Andrew Murray <radarhere@users.noreply.github.com>
2020-02-28 22:33:02 +1100

    If an alpha channel is present, only use that to get bounding box

--- Tests/test_image_getbbox.py
+++ Tests/test_image_getbbox.py
@@ -10,29 +10,32 @@ def test_sanity():
 
 
 def test_bbox():
+    def check(im, fill_color):
+        assert im.getbbox() is None
+
+        im.paste(fill_color, (10, 25, 90, 75))
+        assert im.getbbox() == (10, 25, 90, 75)
+
+        im.paste(fill_color, (25, 10, 75, 90))
+        assert im.getbbox() == (10, 10, 90, 90)
+
+        im.paste(fill_color, (-10, -10, 110, 110))
+        assert im.getbbox() == (0, 0, 100, 100)
 
     # 8-bit mode
     im = Image.new("L", (100, 100), 0)
-    assert im.getbbox() is None
-
-    im.paste(255, (10, 25, 90, 75))
-    assert im.getbbox() == (10, 25, 90, 75)
-
-    im.paste(255, (25, 10, 75, 90))
-    assert im.getbbox() == (10, 10, 90, 90)
-
-    im.paste(255, (-10, -10, 110, 110))
-    assert im.getbbox() == (0, 0, 100, 100)
+    check(im, 255)
 
     # 32-bit mode
     im = Image.new("RGB", (100, 100), 0)
-    assert im.getbbox() is None
+    check(im, 255)
 
-    im.paste(255, (10, 25, 90, 75))
-    assert im.getbbox() == (10, 25, 90, 75)
+    for mode in ("RGBA", "RGBa"):
+        for color in ((0, 0, 0, 0), (127, 127, 127, 0), (255, 255, 255, 0)):
+            im = Image.new(mode, (100, 100), color)
+            check(im, (255, 255, 255, 255))
 
-    im.paste(255, (25, 10, 75, 90))
-    assert im.getbbox() == (10, 10, 90, 90)
-
-    im.paste(255, (-10, -10, 110, 110))
-    assert im.getbbox() == (0, 0, 100, 100)
+    for mode in ("La", "LA", "PA"):
+        for color in ((0, 0), (127, 0), (255, 0)):
+            im = Image.new(mode, (100, 100), color)
+            check(im, (255, 255))
