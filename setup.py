aa2ecc9afe72bd1bc05811b3a32e20d3379c184a
Blaine Bublitz <blaine@tidelift.com>
2020-03-31 13:04:28 -0600

    Update setup.py per suggestion [ci skip]
    Co-Authored-By: Hugo van Kemenade <hugovk@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -858,7 +858,8 @@ try:
         project_urls={
             "Documentation": "https://pillow.readthedocs.io",
             "Source": "https://github.com/python-pillow/Pillow",
-            "Funding": "https://tidelift.com/subscription/pkg/pypi-pillow?utm_source=pypi-pillow&utm_medium=pypi",
+            "Funding": "https://tidelift.com/subscription/pkg/pypi-pillow?"
+            "utm_source=pypi-pillow&utm_medium=pypi",
         },
         classifiers=[
             "Development Status :: 6 - Mature",
