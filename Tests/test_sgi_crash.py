78478dfcfb97378525151507a4ea7aae9484c01b
Hugo <hugovk@users.noreply.github.com>
2020-04-01 10:13:12 +0300

    Black and isort

--- Tests/test_sgi_crash.py
+++ Tests/test_sgi_crash.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
-from .helper import PillowTestCase
 from PIL import Image
 
-repro = ('Tests/images/sgi_overrun_expandrowF04.bin',
-         'Tests/images/sgi_crash.bin',
-         )
+from .helper import PillowTestCase
+
+repro = (
+    "Tests/images/sgi_overrun_expandrowF04.bin",
+    "Tests/images/sgi_crash.bin",
+)
+
 
 class TestSgiCrashes(PillowTestCase):
     def test_crashes(self):
         for path in repro:
-            with open(path, 'rb') as f:
+            with open(path, "rb") as f:
                 im = Image.open(f)
                 with self.assertRaises(IOError):
                     im.load()
