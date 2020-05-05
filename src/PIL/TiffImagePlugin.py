7ecb5aaf7e3dec7ebe170c0fff548a0e44b9196c
Andrew Murray <radarhere@users.noreply.github.com>
2020-05-05 21:20:59 +1000

    BYTE tags of variable length are only single strings

--- src/PIL/TiffImagePlugin.py
+++ src/PIL/TiffImagePlugin.py
@@ -573,8 +573,10 @@ class ImageFileDirectory_v2(MutableMapping):
         # Spec'd length == 1, Actual > 1, Warn and truncate. Formerly barfed.
         # No Spec, Actual length 1, Formerly (<4.2) returned a 1 element tuple.
         # Don't mess with the legacy api, since it's frozen.
-        if (info.length == 1) or (
-            info.length is None and len(values) == 1 and not legacy_api
+        if (
+            (info.length == 1)
+            or self.tagtype[tag] == TiffTags.BYTE
+            or (info.length is None and len(values) == 1 and not legacy_api)
         ):
             # Don't mess with the legacy api, since it's frozen.
             if legacy_api and self.tagtype[tag] in [
