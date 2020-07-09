af5e0fa2aac6504846fad96f212243f70edfe8ff
nulano <nulano@nulano.eu>
2020-07-09 23:20:45 +0200

    add Python stdlib intersphinx mapping

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,8 @@ extensions = [
     "sphinx_removed_in",
 ]
 
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
