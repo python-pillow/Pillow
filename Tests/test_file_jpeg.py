1fd9ccfe3c38fdca9abcb90fed91bc5dab8a4b05
luphord <luphord@protonmail.com>
2020-07-24 16:30:47 +0200

    test Exif self-equality

--- Tests/test_file_jpeg.py
+++ Tests/test_file_jpeg.py
@@ -241,6 +241,15 @@ class TestFileJpeg:
 
         # Assert
         assert exif[gps_index] == expected_exif_gps
+    
+    def test_exif_equality(self):
+        # in 7.2.0 Exif rationals are read as TiffImagePlugin.IFDRational
+        # which broke self-equality of Exif data
+        exifs = []
+        for i in range(2):
+            with Image.open("Tests/images/exif-200dpcm.jpg") as im:
+                exifs.append(im._getexif())
+        assert exifs[0] == exifs[1]
 
     def test_exif_rollback(self):
         # rolling back exif support in 3.1 to pre-3.0 formatting.
