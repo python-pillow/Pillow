0af193afc0a66545667b651444256234f63ad1fe
Andrew Murray <radarhere@users.noreply.github.com>
2020-08-04 21:58:03 +1000

    Added test

--- Tests/test_imagefile.py
+++ Tests/test_imagefile.py
@@ -244,3 +244,8 @@ class TestPyDecoder:
         im = MockImageFile(buf)
         assert im.format is None
         assert im.get_format_mimetype() is None
+
+    def test_oserror(self):
+        im = Image.new("RGB", (1, 1))
+        with pytest.raises(OSError):
+            im.save(BytesIO(), "JPEG2000")
