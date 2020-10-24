39a366203c22543cf1a147a1de7510bd8e40b657
nulano <nulano@nulano.eu>
2020-10-24 02:17:04 +0100

    run both tagged and latest

---
+++ .github/workflows/build.sh
@@ -0,0 +1,33 @@
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  # these cause a conflict with built webp and libtiff
+  brew remove --ignore-dependencies webp zstd xz libtiff
+fi
+
+if [[ "$MB_PYTHON_VERSION" == "pypy3.6-7.3" ]]; then
+  if [[ "$TRAVIS_OS_NAME" != "macos-latest" ]]; then
+    MB_ML_VER="2010"
+    DOCKER_TEST_IMAGE="multibuild/xenial_$PLAT"
+  else
+    MB_PYTHON_OSX_VER="10.9"
+  fi
+fi
+
+echo "::group::Install a virtualenv"
+  source multibuild/common_utils.sh
+  source multibuild/travis_steps.sh
+  # can't use default 7.3.1 on macOS due to https://foss.heptapod.net/pypy/pypy/-/issues/3229
+  LATEST_PP_7p3=7.3.2
+  pip install virtualenv
+  before_install
+echo "::endgroup::"
+
+echo "::group::Build wheel"
+  clean_code $REPO_DIR $BUILD_COMMIT
+  build_wheel $REPO_DIR $PLAT
+  ls -l "${GITHUB_WORKSPACE}/${WHEEL_SDIR}/"
+echo "::endgroup::"
+
+echo "::group::Test wheel"
+  install_run $PLAT
+echo "::endgroup::"
