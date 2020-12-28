59ed81f8387b1491890aa20482e2f1776ecf9970
Eric Soroos <eric-github@soroos.net>
2020-12-28 13:48:46 +0100

    Add pytest configuration for patching around an unknown valgrind mark

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -10,3 +10,18 @@ def pytest_report_header(config):
             return out.getvalue()
     except Exception as e:
         return f"pytest_report_header failed: {e}"
+
+def pytest_configure(config):
+    # We're marking some tests to ignore valgrind errors and XFAIL them.
+    # Ensure that the mark is defined even in cases where pytest-valgrind isn't installed
+
+    import pytest
+    import warnings
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        try:
+            getattr(pytest.mark, "valgrind_known_error")
+        except:
+            config.addinivalue_line("markers",
+                                    "valgrind_known_error: Tests that have known issues with valgrind")
