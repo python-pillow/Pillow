b1b7d2ff182251180b51729e500c0518e705559f
UrielMaD <urielmd00@gmail.com>
2020-12-27 21:37:15 -0600

    XMP feature added

--- Tests/test_image_getxmp.py
+++ Tests/test_image_getxmp.py
@@ -1,9 +1,9 @@
-from .helper import hopper
+from PIL import Image
 
 
 def test_getxmp():
-    im = hopper()
+    im = Image.open("Tests/images/hopper.jpg")
     type_repr = repr(type(im.getxmp()))
 
-    
+
     assert "dict" in type_repr
