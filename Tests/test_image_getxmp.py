7968279c13b126c98d1ada52fc1b5343f563a5a2
UrielMaD <urielmd00@gmail.com>
2020-12-27 22:24:38 -0600

    XMP feature added

--- Tests/test_image_getxmp.py
+++ Tests/test_image_getxmp.py
@@ -2,7 +2,8 @@ from PIL import Image
 
 
 def test_getxmp():
-    im = Image.open("Tests/images/hopper.jpg")
+    im = Image.open("Tests/images/xmp_test.jpg")
     type_repr = repr(type(im.getxmp()))
 
     assert "dict" in type_repr
+    assert isinstance(im.getxmp()['Description'][0]['Version'], int)
