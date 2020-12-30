d35995f945398d9c7cdefcae24d7fea889916116
Andrew Murray <radarhere@users.noreply.github.com>
2020-12-30 14:41:22 +1100

    Lint fixes

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -11,17 +11,22 @@ def pytest_report_header(config):
     except Exception as e:
         return f"pytest_report_header failed: {e}"
 
+
 def pytest_configure(config):
     # We're marking some tests to ignore valgrind errors and XFAIL them.
-    # Ensure that the mark is defined even in cases where pytest-valgrind isn't installed
+    # Ensure that the mark is defined
+    # even in cases where pytest-valgrind isn't installed
+
+    import warnings
 
     import pytest
-    import warnings
 
     with warnings.catch_warnings():
         warnings.simplefilter("error")
         try:
             getattr(pytest.mark, "valgrind_known_error")
         except:
-            config.addinivalue_line("markers",
-                                    "valgrind_known_error: Tests that have known issues with valgrind")
+            config.addinivalue_line(
+                "markers",
+                "valgrind_known_error: Tests that have known issues with valgrind",
+            )
