06b7b97a7aa771761d746e5a6fe424b543baa231
Andrew Murray <radarhere@users.noreply.github.com>
2020-12-30 22:39:49 +1100

    Use isinstance to check type

--- Tests/test_image_getxmp.py
+++ Tests/test_image_getxmp.py
@@ -3,7 +3,8 @@ from PIL import Image
 
 def test_getxmp():
     with Image.open("Tests/images/xmp_test.jpg") as im:
-        type_repr = repr(type(im.getxmp()))
+        xmp = im.getxmp()
 
-        assert "dict" in type_repr
-        assert isinstance(im.getxmp()["Description"][0]["Version"], str)
+        assert isinstance(xmp, dict)
+        assert isinstance(xmp["Description"][0]["Version"], str)
+
