17c48db318a7e48d16909f08973df94b27dcc736
Andrew Murray <radarhere@users.noreply.github.com>
2020-12-30 22:35:54 +1100

    Use context manager

--- Tests/test_image_getxmp.py
+++ Tests/test_image_getxmp.py
@@ -2,8 +2,8 @@ from PIL import Image
 
 
 def test_getxmp():
-    im = Image.open("Tests/images/xmp_test.jpg")
-    type_repr = repr(type(im.getxmp()))
+    with Image.open("Tests/images/xmp_test.jpg") as im:
+        type_repr = repr(type(im.getxmp()))
 
-    assert "dict" in type_repr
-    assert isinstance(im.getxmp()["Description"][0]["Version"], str)
+        assert "dict" in type_repr
+        assert isinstance(im.getxmp()["Description"][0]["Version"], str)
