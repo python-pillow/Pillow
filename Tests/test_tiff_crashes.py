3fee28eb9479bf7d59e0fa08068f9cc4a6e2f04c
Eric Soroos <eric-github@soroos.net>
2021-01-03 21:35:32 +0100

    Incorrect error code checking in TiffDecode.c
    * since Pillow 8.1.0
    * CVE-2021-25289

--- Tests/test_tiff_crashes.py
+++ Tests/test_tiff_crashes.py
@@ -24,6 +24,8 @@ from .helper import on_ci
         "Tests/images/crash_1.tif",
         "Tests/images/crash_2.tif",
         "Tests/images/crash-2020-10-test.tif",
+        "Tests/images/crash-1152ec2d1a1a71395b6f2ce6721c38924d025bf3.tif",
+        "Tests/images/crash-0e16d3bfb83be87356d026d66919deaefca44dac.tif",
     ],
 )
 @pytest.mark.filterwarnings("ignore:Possibly corrupt EXIF data")
