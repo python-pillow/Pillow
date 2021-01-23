8b8076bdcb3815be0ef0d279651d8d1342b8ea61
Eric Soroos <eric-github@soroos.net>
2021-01-23 11:36:50 +0100

    Fix for CVE-2021-25291
    * Invalid tile boundaries lead to OOB Read in TiffDecode.c, in TiffReadRGBATile
    * Check the tile validity before attempting to read.

--- src/libImaging/TiffDecode.c
+++ src/libImaging/TiffDecode.c
@@ -479,6 +479,15 @@ int ImagingLibTiffDecode(Imaging im, ImagingCodecState state, UINT8* buffer, Py_
 
         for (y = state->yoff; y < state->ysize; y += tile_length) {
             for (x = state->xoff; x < state->xsize; x += tile_width) {
+                /* Sanity Check. Apparently in some cases, the TiffReadRGBA* functions
+                   have a different view of the size of the tiff than we're getting from
+                   other functions. So, we need to check here. 
+                */
+                if (!TIFFCheckTile(tiff, x, y, 0, 0)) {
+                    TRACE(("Check Tile Error, Tile at %dx%d\n", x, y));
+                    state->errcode = IMAGING_CODEC_BROKEN;
+                    goto decode_err;
+                }
                 if (isYCbCr) {
                     /* To avoid dealing with YCbCr subsampling, let libtiff handle it */
                     if (!TIFFReadRGBATile(tiff, x, y, (UINT32 *)state->buffer)) {
