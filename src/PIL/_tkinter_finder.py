cf98f178ad2bb55261a8ea41003ada97e520d306
Andrew Murray <radarhere@users.noreply.github.com>
2021-01-26 08:01:26 +1100

    Added tk version

--- src/PIL/_tkinter_finder.py
+++ src/PIL/_tkinter_finder.py
@@ -1,9 +1,12 @@
 """ Find compiled module linking to Tcl / Tk libraries
 """
 import sys
+import tkinter
 from tkinter import _tkinter as tk
 
 if hasattr(sys, "pypy_find_executable"):
     TKINTER_LIB = tk.tklib_cffi.__file__
 else:
     TKINTER_LIB = tk.__file__
+
+tk_version = str(tkinter.TkVersion)
