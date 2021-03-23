8913166c7ea748d6884813172208eac4cb057141
elejke <german.novikov@phystech.edu>
2021-03-23 11:59:37 +0300

    fix test function name for autocontrast

--- Tests/test_imageops.py
+++ Tests/test_imageops.py
@@ -337,7 +337,7 @@ def test_autocontrast_mask_toy_input():
         assert ImageStat.Stat(result_nomask).median == [128]
 
 
-def test_auto_contrast_mask_real_input():
+def test_autocontrast_mask_real_input():
     # Test the autocontrast with a rectangular mask
     with Image.open("Tests/images/iptc.jpg") as img:
 
