56dfc4a95607a7c6cfcf908a680cc796cb21a281
Andrew Murray <radarhere@users.noreply.github.com>
2021-04-05 19:01:13 +1000

    Added 301 and 532 to core tags

--- src/PIL/TiffTags.py
+++ src/PIL/TiffTags.py
@@ -485,9 +485,6 @@ LIBTIFF_CORE = {
     65537,
 }
 
-LIBTIFF_CORE.remove(301)  # Array of short, crashes
-LIBTIFF_CORE.remove(532)  # Array of long, crashes
-
 LIBTIFF_CORE.remove(255)  # We don't have support for subfiletypes
 LIBTIFF_CORE.remove(322)  # We don't have support for writing tiled images with libtiff
 LIBTIFF_CORE.remove(323)  # Tiled images
