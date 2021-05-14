f132e4de8db924eb9bc4df35d30101db16e20583
Andrew Murray <radarhere@users.noreply.github.com>
2021-05-14 19:48:47 +1000

    Removed setuptools version requirement

--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -17,9 +17,5 @@ echo -e "[openblas]\nlibraries = openblas\nlibrary_dirs = /usr/local/opt/openbla
 # TODO Remove condition when numpy supports 3.10
 if ! [ "$GHA_PYTHON_VERSION" == "3.10-dev" ]; then python3 -m pip install numpy ; fi
 
-# TODO Remove when 3.8 / 3.9 includes setuptools 49.3.2+:
-if [ "$GHA_PYTHON_VERSION" == "3.8" ]; then python3 -m pip install -U "setuptools>=49.3.2" ; fi
-if [ "$GHA_PYTHON_VERSION" == "3.9" ]; then python3 -m pip install -U "setuptools>=49.3.2" ; fi
-
 # extra test images
 pushd depends && ./install_extra_test_images.sh && popd
