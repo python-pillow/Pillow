461628845d3c9ca33bb230603ef5e87aeb4ead50
Andrew Murray <radarhere@users.noreply.github.com>
2021-06-28 22:12:38 +1000

    Disposal method 2 is transparency or black when saving

--- src/PIL/GifImagePlugin.py
+++ src/PIL/GifImagePlugin.py
@@ -461,10 +461,10 @@ def _write_multiple_frames(im, fp, palette):
                 previous = im_frames[-1]
                 if encoderinfo.get("disposal") == 2:
                     if background_im is None:
-                        background = _get_background(
-                            im,
-                            im.encoderinfo.get("background", im.info.get("background")),
+                        color = im.encoderinfo.get(
+                            "transparency", im.info.get("transparency", (0, 0, 0))
                         )
+                        background = _get_background(im_frame, color)
                         background_im = Image.new("P", im_frame.size, background)
                         background_im.putpalette(im_frames[0]["im"].palette)
                     base_im = background_im
