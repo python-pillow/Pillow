370f32ba720a446d9e53842f0ebaed96bfa31478
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-07-29 12:04:48 +0300

    Install wheel on CI to avoid legacy setup.py installs

--- .ci/install.sh
+++ .ci/install.sh
@@ -22,6 +22,7 @@ sudo apt-get -qq install libfreetype6-dev liblcms2-dev python3-tk\
                          cmake imagemagick libharfbuzz-dev libfribidi-dev
 
 python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade wheel
 PYTHONOPTIMIZE=0 python3 -m pip install cffi
 python3 -m pip install coverage
 python3 -m pip install defusedxml
