c5ac9326388d3f9995699ca201c82d242d7f03ab
Andrew Murray <radarhere@users.noreply.github.com>
2021-08-18 21:55:12 +1000

    Updated harfbuzz to 2.9.0

--- winbuild/build_prepare.py
+++ winbuild/build_prepare.py
@@ -277,9 +277,9 @@ deps = {
         "libs": [r"*.lib"],
     },
     "harfbuzz": {
-        "url": "https://github.com/harfbuzz/harfbuzz/archive/2.8.2.zip",
-        "filename": "harfbuzz-2.8.2.zip",
-        "dir": "harfbuzz-2.8.2",
+        "url": "https://github.com/harfbuzz/harfbuzz/archive/2.9.0.zip",
+        "filename": "harfbuzz-2.9.0.zip",
+        "dir": "harfbuzz-2.9.0",
         "build": [
             cmd_cmake("-DHB_HAVE_FREETYPE:BOOL=TRUE"),
             cmd_nmake(target="clean"),
