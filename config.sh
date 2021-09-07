a0e13805d3e4d4f18ab7ca2ef8c5ad6b2dad050d
Andrew Murray <radarhere@users.noreply.github.com>
2021-09-08 07:46:21 +1000

    Updated harfbuzz to 2.9.1

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ ARCHIVE_SDIR=pillow-depends-master
 
 # Package versions for fresh source builds
 FREETYPE_VERSION=2.11.0
-HARFBUZZ_VERSION=2.9.0
+HARFBUZZ_VERSION=2.9.1
 LIBPNG_VERSION=1.6.37
 ZLIB_VERSION=1.2.11
 JPEG_VERSION=9d
