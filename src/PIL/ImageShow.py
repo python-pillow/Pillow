ee16ffc37d476147f673f010976f87b201690ced
Mihail Shinder <mihail@shinder.ml>
2021-11-19 10:57:41 +0200

    FIX: Membership check should be

--- src/PIL/ImageShow.py
+++ src/PIL/ImageShow.py
@@ -271,10 +271,8 @@ class CommandViewer(Viewer):
         :param options: kwargs that should have 'command' in it
         """
 
-        if not "command" in options:
-            raise TypeError(
-                "CommandViewer missing required keyword-only argument 'command'"
-            )
+        if "command" not in options:
+            raise TypeError("CommandViewer missing required keyword-only argument 'command'")
         command = options["command"]
         if not isinstance(command, str):
             raise TypeError(f"'command' must be 'str' not '{type(command)}'")
