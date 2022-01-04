4e4dc1dc1515d476ccc1918cae7ed8208cd4ea0c
Alexander Karpinsky <homm86@gmail.com>
2022-01-04 16:55:13 +0300

    Protect double inclusion of ImagingSIMD.h

--- src/libImaging/ImagingSIMD.h
+++ src/libImaging/ImagingSIMD.h
@@ -1,13 +1,18 @@
+#ifndef __IMAGING_SIMD_H__
+#define __IMAGING_SIMD_H__
+
 /* Microsoft compiler doesn't limit intrinsics for an architecture.
    This macro is set only on x86 and means SSE2 and above including AVX2. */
 #if defined(_M_X64) || _M_IX86_FP == 2
     #define __SSE4_2__
 #endif
+
 #if defined(__SSE4_2__)
     #include <emmintrin.h>
     #include <mmintrin.h>
     #include <smmintrin.h>
 #endif
+
 #if defined(__AVX2__)
     #include <immintrin.h>
 #endif
@@ -24,4 +29,6 @@ static __m256i inline
 mm256_cvtepu8_epi32(void *ptr) {
     return _mm256_cvtepu8_epi32(_mm_loadl_epi64((__m128i *) ptr));
 }
+#endif
+
 #endif
