9d8f173e39458bb73107f90813c745717d87ea78
Andrew Murray <radarhere@users.noreply.github.com>
2022-02-03 18:05:50 +1100

    Updated libimagequant to 4.0.0

--- depends/install_imagequant.sh
+++ depends/install_imagequant.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # install libimagequant
 
-archive=libimagequant-2.17.0
+archive=libimagequant-4.0.0
 
 ./download-and-extract.sh $archive https://raw.githubusercontent.com/python-pillow/pillow-depends/main/$archive.tar.gz
 
-pushd $archive
+pushd $archive/imagequant-sys
 
-make shared
-sudo cp libimagequant.so* /usr/lib/
-sudo cp libimagequant.h /usr/include/
+cargo install cargo-c
+cargo cinstall --prefix=/usr --destdir=.
+sudo cp usr/lib/libimagequant.so* /usr/lib/
+sudo cp usr/include/libimagequant.h /usr/include/
 
 popd
