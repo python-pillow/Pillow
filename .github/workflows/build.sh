a1543bcdad3c0d44b70d780a2b12451a9ed22a29
Andrew Murray <radarhere@users.noreply.github.com>
2022-02-04 11:32:55 +1100

    Do not remove libxdmcp

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,9 +1,9 @@
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  # webp, zstd, xz, libtiff, libxcb, libxdmcp cause a conflict with building webp, libtiff, libxcb
+  # webp, zstd, xz, libtiff, libxcb cause a conflict with building webp, libtiff, libxcb
   # curl from brew requires zstd, use system curl
   # if php is installed, brew tries to reinstall these after installing openblas
-  brew remove --ignore-dependencies webp zstd xz libtiff libxcb libxdmcp curl php
+  brew remove --ignore-dependencies webp zstd xz libtiff libxcb curl php
 fi
 
 if [[ "$MB_PYTHON_VERSION" == pypy3* ]]; then
