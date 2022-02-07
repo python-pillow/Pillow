4d0e294eb0b5ede4a4c92293aa4ef2794c3b6025
DWesl <22566757+DWesl@users.noreply.github.com>
2022-02-07 17:00:37 -0500

    CI: Revert changes to test.sh
    Some early runs needed to explicitly specify the minor version to avoid calling python3.9 which had none of the dependencies installed.
    That problem should be fixed.

--- .ci/test.sh
+++ .ci/test.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-python3$1 -c "from PIL import Image"
+python3 -c "from PIL import Image"
 
-python3$1 -bb -m pytest -v -x -W always --cov PIL --cov Tests --cov-report term Tests $REVERSE
+python3 -bb -m pytest -v -x -W always --cov PIL --cov Tests --cov-report term Tests $REVERSE
