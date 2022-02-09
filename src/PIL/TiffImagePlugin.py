601c9d8515dba996af3f0b96d1a671619de37f10
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-02-09 14:28:43 +0200

    Fix return in docs

--- src/PIL/TiffImagePlugin.py
+++ src/PIL/TiffImagePlugin.py
@@ -1124,6 +1124,7 @@ class TiffImageFile(ImageFile.ImageFile):
         """
         Returns a dictionary containing the XMP tags.
         Requires defusedxml to be installed.
+
         :returns: XMP tags in a dictionary.
         """
         return self._getxmp(self.tag_v2[700]) if 700 in self.tag_v2 else {}
