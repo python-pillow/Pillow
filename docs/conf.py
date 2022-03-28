3ce484bd3de77221879f62b1c314f91e473e9f65
Andrew Murray <radarhere@users.noreply.github.com>
2022-03-28 21:41:44 +1100

    Use different logos for light and dark mode

--- docs/conf.py
+++ docs/conf.py
@@ -129,7 +129,10 @@ html_theme = "furo"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    "light_logo": "pillow-logo-dark-text.png",
+    "dark_logo": "pillow-logo.png",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -143,7 +146,7 @@ html_theme = "furo"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "resources/pillow-logo.png"
+# html_logo = "resources/pillow-logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
