7605e4601161da7f392d5db55559aade4d82565b
DWesl <22566757+DWesl@users.noreply.github.com>
2022-04-23 08:22:33 -0400

    CI: Set shell for Cygwin CI steps
    Allows me to avoid .ci/build_cygwin.sh

--- .ci/build_cygwin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-python3 -m coverage erase
-make clean
-CFLAGS="-coverage -Werror=implicit-function-declaration" python3 -m pip install -v --global-option="build_ext" .
-python3 selftest.py
