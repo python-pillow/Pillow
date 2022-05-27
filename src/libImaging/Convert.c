6a071f81d798bd53dbff7910c2165a876281278a
Davide Consalvo <dcdavide40@gmail.com>
2022-05-27 12:24:19 +0200

    fixed p2pa conversion

--- src/libImaging/Convert.c
+++ src/libImaging/Convert.c
@@ -1031,7 +1031,7 @@ p2pa(UINT8 *out, const UINT8 *in, int xsize, ImagingPalette palette) {
     int x;
     int rgb = strcmp(palette->mode, "RGB");
     for (x = 0; x < xsize; x++, in++) {
-        const UINT8 *rgba = &palette->palette[in[0]];
+        const UINT8 *rgba = &palette->palette[in[0] * 4];
         *out++ = in[0];
         *out++ = in[0];
         *out++ = in[0];
