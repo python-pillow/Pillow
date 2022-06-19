607f5080808d38dbc5e1d89c18ffec5d77f6f4b9
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-06-19 12:29:39 +0300

    Update comment
    We get "an integer is required (got type NoneType)" with Python 3.7-3.9 on Windows.
    
    We get "'NoneType' object cannot be interpreted as an integer" with Python 3.10 on Windows and all versions on macOS and Ubuntu.

--- Tests/test_imagecms.py
+++ Tests/test_imagecms.py
@@ -189,8 +189,8 @@ def test_exceptions():
         ImageCms.getProfileName(None)
     skip_missing()
 
-    # macOS/Ubuntu: "'NoneType' object cannot be interpreted as an integer"
-    # Windows: "an integer is required (got type NoneType)"
+    # Windows, Python <= 3.9: "an integer is required (got type NoneType)"
+    # Others: "'NoneType' object cannot be interpreted as an integer"
     with pytest.raises(ImageCms.PyCMSError, match="integer"):
         ImageCms.isIntentSupported(SRGB, None, None)
 
