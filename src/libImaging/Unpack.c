31ad2bc133284af89ff649d1cd4f80af8d2b739b
Yay295 <Yay295@gmail.com>
2022-08-21 17:44:11 -0500

    declare loop variable outside loop

--- src/libImaging/Unpack.c
+++ src/libImaging/Unpack.c
@@ -414,10 +414,11 @@ unpackL4IR(UINT8 *out, const UINT8 *in, int pixels) {
 
 static void
 unpackLAL(UINT8 *out, const UINT8 *in, int pixels) {
+    int i;
     UINT8 *in1 = in;
     UINT8 *in2 = in + pixels;
     /* LA, line interleaved */
-    for (int i = 0; i < pixels; ++i) {
+    for (i = 0; i < pixels; ++i) {
         *out++ = *in1++;
         *out++ = *in2++;
     }
