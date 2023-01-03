ea9a1b84aa5a8f6ab077974a052e54ff52ee2c50
Andrew Murray <radarhere@users.noreply.github.com>
2023-01-03 14:09:45 +1100

    LOAD_TRUNCATED_IMAGES may allow PNG images to open

--- src/PIL/__init__.py
+++ src/PIL/__init__.py
@@ -75,6 +75,10 @@ _plugins = [
 class UnidentifiedImageError(OSError):
     """
     Raised in :py:meth:`PIL.Image.open` if an image cannot be opened and identified.
+
+    If a PNG image raises this error, setting :data:`.ImageFile.LOAD_TRUNCATED_IMAGES`
+    to true may allow the image to be opened after all. The setting will ignore missing
+    data and checksum failures.
     """
 
     pass
