58719fb16059cdb6d107203b4c5d342bfd9a52de
Yay295 <Yay295@users.noreply.github.com>
2023-01-02 21:38:58 -0600

    fix function names for pytest

--- Tests/test_uploader.py
+++ Tests/test_uploader.py
@@ -1,13 +1,13 @@
 from .helper import assert_image_equal, assert_image_similar, hopper
 
 
-def check_upload_equal():
+def test_upload_equal():
     result = hopper("P").convert("RGB")
     target = hopper("RGB")
     assert_image_equal(result, target)
 
 
-def check_upload_similar():
+def test_upload_similar():
     result = hopper("P").convert("RGB")
     target = hopper("RGB")
     assert_image_similar(result, target, 0)
