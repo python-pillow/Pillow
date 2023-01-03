56680b253c681601692a1b215e1b8ed846469923
Yay295 <Yay295@gmail.com>
2023-01-03 00:23:45 -0600

    set verbosity to 0 before comparing image bytes

--- conftest.py
+++ conftest.py
@@ -1,1 +1,12 @@
 pytest_plugins = ["Tests.helper"]
+
+
+def pytest_configure(config):
+    """
+    Keep a reference to pytest's configuration in our helper class.
+    This function is called by pytest each time the configuration is updated.
+    https://docs.pytest.org/en/latest/reference/reference.html#pytest.hookspec.pytest_configure
+    """
+    import Tests.helper
+
+    Tests.helper.pytestconfig = config
