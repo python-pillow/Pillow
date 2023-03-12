cdb9ca9ea1f08b860a917b55f56c8bfa69071d6b
Andrew Murray <radarhere@users.noreply.github.com>
2023-03-12 23:28:29 +1100

    Removed class

--- Tests/test_file_qoi.py
+++ Tests/test_file_qoi.py
@@ -5,24 +5,24 @@ from PIL import Image, QoiImagePlugin
 from .helper import assert_image_equal_tofile, assert_image_similar_tofile
 
 
-class TestFileQOI:
-    def test_sanity(self):
-        with Image.open("Tests/images/hopper.qoi") as im:
-            assert im.mode == "RGB"
-            assert im.size == (128, 128)
-            assert im.format == "QOI"
+def test_sanity():
+    with Image.open("Tests/images/hopper.qoi") as im:
+        assert im.mode == "RGB"
+        assert im.size == (128, 128)
+        assert im.format == "QOI"
 
-            assert_image_equal_tofile(im, "Tests/images/hopper.png")
+        assert_image_equal_tofile(im, "Tests/images/hopper.png")
 
-        with Image.open("Tests/images/pil123rgba.qoi") as im:
-            assert im.mode == "RGBA"
-            assert im.size == (162, 150)
-            assert im.format == "QOI"
+    with Image.open("Tests/images/pil123rgba.qoi") as im:
+        assert im.mode == "RGBA"
+        assert im.size == (162, 150)
+        assert im.format == "QOI"
 
-            assert_image_similar_tofile(im, "Tests/images/pil123rgba.png", 0.03)
+        assert_image_similar_tofile(im, "Tests/images/pil123rgba.png", 0.03)
 
-    def test_invalid_file(self):
-        invalid_file = "Tests/images/flower.jpg"
 
-        with pytest.raises(SyntaxError):
-            QoiImagePlugin.QoiImageFile(invalid_file)
+def test_invalid_file():
+    invalid_file = "Tests/images/flower.jpg"
+
+    with pytest.raises(SyntaxError):
+        QoiImagePlugin.QoiImageFile(invalid_file)
