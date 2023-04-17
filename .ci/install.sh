aa2e662995eaf67e2f9f53d6817a173c12b45a19
rrcgat <rrcgat@gmail.com>
2023-04-17 16:44:43 +0800

    Add sway and wl-clipboard dependencies to GitHub CI workflow

--- .ci/install.sh
+++ .ci/install.sh
@@ -22,7 +22,8 @@ set -e
 if [[ $(uname) != CYGWIN* ]]; then
     sudo apt-get -qq install libfreetype6-dev liblcms2-dev python3-tk\
                              ghostscript libffi-dev libjpeg-turbo-progs libopenjp2-7-dev\
-                             cmake meson imagemagick libharfbuzz-dev libfribidi-dev
+                             cmake meson imagemagick libharfbuzz-dev libfribidi-dev\
+                             sway wl-clipboard
 fi
 
 python3 -m pip install --upgrade pip
