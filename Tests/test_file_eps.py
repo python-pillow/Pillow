411ff3322cef380cdf61af67c35c5681584c70c0
Frederick Price <fprice@pricemail.ca>
2023-04-20 12:45:24 -0400

    Add in cherry-pick

--- Tests/test_file_eps.py
+++ Tests/test_file_eps.py
@@ -1,6 +1,7 @@
 import io
 
 from PIL import EpsImagePlugin, Image
+import pytest
 
 from .helper import PillowTestCase, hopper, unittest
 
@@ -252,3 +253,17 @@ class TestFileEps(PillowTestCase):
         self.assertEqual(image.mode, "RGB")
         self.assertEqual(image.size, (460, 352))
         self.assertEqual(image.format, "EPS")
+
+
+@pytest.mark.timeout(timeout=5)
+@pytest.mark.parametrize(
+    "test_file",
+    [
+        ("Tests/images/timeout-d675703545fee17acab56e5fec644c19979175de.eps")
+    ],
+)
+def test_timeout(test_file):
+    with open(test_file, "rb") as f:
+        with pytest.raises(Image.UnidentifiedImageError):
+            with Image.open(f):
+                pass
