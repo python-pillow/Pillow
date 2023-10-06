57c1cf603e13d9aeeea136c7e6160c7c0e07aae1
Andrew Murray <radarhere@users.noreply.github.com>
2023-10-07 01:15:04 +1100

    Fixed may be used uninitialized warning

--- src/_imaging.c
+++ src/_imaging.c
@@ -1575,11 +1575,7 @@ if (PySequence_Check(op)) { \
             }
             double value;
             if (image->bands == 1) {
-                int bigendian;
-                if (image->type == IMAGING_TYPE_SPECIAL) {
-                    // I;16*
-                    bigendian = strcmp(image->mode, "I;16B") == 0;
-                }
+                int bigendian = image->type == IMAGING_TYPE_SPECIAL && strcmp(image->mode, "I;16B") == 0;
                 for (i = x = y = 0; i < n; i++) {
                     set_value_to_item(seq, i);
                     if (scale != 1.0 || offset != 0.0) {
