b273599e0f3d0312f329c482978c51cee67462d4
Nulano <nulano@nulano.eu>
2023-10-09 19:49:50 +0200

    fix testing pypy

--- winbuild/test_docker.ps1
+++ winbuild/test_docker.ps1
@@ -1,17 +1,19 @@
 param ([string]$python)
 $ErrorActionPreference  = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 if ($python -like "pypy*") {
-    $url = 'https://downloads.python.org/pypy/{0}-v7.3.11-win64.zip' -f $python
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    echo "Downloading https://aka.ms/vs/15/release/vc_redist.x64.exe"
     Invoke-WebRequest -Uri 'https://aka.ms/vs/15/release/vc_redist.x64.exe' -OutFile 'vc_redist.x64.exe'
-    & C:\vc_redist.x64.exe /install /quiet /norestart
+    C:\vc_redist.x64.exe /install /quiet /norestart | Out-Null
+    $url = 'https://downloads.python.org/pypy/{0}-v7.3.13-win64.zip' -f $python
+    echo "Downloading $url"
     Invoke-WebRequest -Uri $url -OutFile 'pypy.zip'
     tar -xf 'pypy.zip'
     mv pypy*-win64 C:\Python
 } else {
     $url = 'https://www.python.org/ftp/python/{0}.0/python-{0}.0-amd64.exe' -f $python
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    echo "Downloading $url"
     Invoke-WebRequest -Uri $url -OutFile 'setup.exe'
     Start-Process setup.exe -Wait -NoNewWindow -PassThru -ArgumentList @(
             '/quiet',
@@ -29,6 +31,7 @@ $env:path += ";C:\Python\;C:\pillow\winbuild\build\bin\"
 cd C:\pillow
 & python -VV
 & python -m ensurepip
+& python -m pip install -U pip
 & python -m pip install pytest pytest-timeout
 & python -m pip install "$(Get-ChildItem *.whl -Name)"
 & python -m pytest -vx Tests\check_wheel.py Tests
