dcc66597f0824cf980211eff4fd56c6e1c7754ac
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-11-08 13:58:58 +0200

    Test Python 3.13 pre-release

--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -5,7 +5,9 @@ set -e
 brew install libtiff libjpeg openjpeg libimagequant webp little-cms2 freetype libraqm
 export PKG_CONFIG_PATH="/usr/local/opt/openblas/lib/pkgconfig"
 
-PYTHONOPTIMIZE=0 python3 -m pip install cffi
+# TODO Update condition when cffi supports 3.13
+if ! [[ "$GHA_PYTHON_VERSION" == "3.13" ]]; then PYTHONOPTIMIZE=0 python3 -m pip install cffi ; fi
+
 python3 -m pip install coverage
 python3 -m pip install defusedxml
 python3 -m pip install olefile
@@ -14,7 +16,8 @@ python3 -m pip install -U pytest-cov
 python3 -m pip install -U pytest-timeout
 python3 -m pip install pyroma
 
-python3 -m pip install numpy
+# TODO Update condition when NumPy supports 3.13
+if ! [[ "$GHA_PYTHON_VERSION" == "3.13" ]]; then python3 -m pip install numpy ; fi
 
 # extra test images
 pushd depends && ./install_extra_test_images.sh && popd
