396b96f28b1ea3037720a5279c85cf0fdeb5a015
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-12-26 15:08:58 -0700

    Optimise ImageMode.getmode
    Co-authored-by: Andrew Murray <3112309+radarhere@users.noreply.github.com>

--- src/PIL/ImageMode.py
+++ src/PIL/ImageMode.py
@@ -42,7 +42,6 @@ class ModeDescriptor:
 @lru_cache
 def getmode(mode: str) -> ModeDescriptor:
     """Gets a mode descriptor for the given mode."""
-    # if not _modes:
     # initialize mode cache
     endian = "<" if sys.byteorder == "little" else ">"
 
