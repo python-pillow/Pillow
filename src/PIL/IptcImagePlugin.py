fa4b3776f0dda812a1cea15f3ba17a3777b8c5b6
Nulano <nulano@nulano.eu>
2023-12-30 00:02:34 +0100

    ﻿deprecate IptcImagePlugin.{dump,i,PAD}

--- src/PIL/IptcImagePlugin.py
+++ src/PIL/IptcImagePlugin.py
@@ -22,14 +22,25 @@ import tempfile
 from . import Image, ImageFile
 from ._binary import i16be as i16
 from ._binary import i32be as i32
+from ._deprecate import deprecate
 
 COMPRESSION = {1: "raw", 5: "jpeg"}
 
-PAD = b"\0\0\0\0"
+
+def __getattr__(name):
+    if name == "PAD":
+        deprecate("IptcImagePlugin.PAD", 12)
+        return b"\0\0\0\0"
+    msg = f"module '{__name__}' has no attribute '{name}'"
+    raise AttributeError(msg)
 
 
 #
 # Helpers
+
+
+def _i(c):
+    return i32((b"\0\0\0\0" + c)[-4:])
 
 
 def _i8(c: int | bytes) -> int:
@@ -37,10 +48,12 @@ def _i8(c: int | bytes) -> int:
 
 
 def i(c):
-    return i32((PAD + c)[-4:])
+    deprecate("IptcImagePlugin.i", 12)
+    return _i(c)
 
 
 def dump(c):
+    deprecate("IptcImagePlugin.dump", 12)
     for i in c:
         print("%02x" % _i8(i), end=" ")
     print()
@@ -56,7 +69,7 @@ class IptcImageFile(ImageFile.ImageFile):
     format_description = "IPTC/NAA"
 
     def getint(self, key):
-        return i(self.info[key])
+        return _i(self.info[key])
 
     def field(self):
         #
@@ -80,7 +93,7 @@ class IptcImageFile(ImageFile.ImageFile):
         elif size == 128:
             size = 0
         elif size > 128:
-            size = i(self.fp.read(size - 128))
+            size = _i(self.fp.read(size - 128))
         else:
             size = i16(s, 3)
 
