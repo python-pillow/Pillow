a09e056a4ca9ab5283b14bfd9ccec9aeb0757643
Andrew Murray <radarhere@users.noreply.github.com>
2024-01-22 18:42:43 +1100

    Added type hints

--- Tests/test_font_pcf_charsets.py
+++ Tests/test_font_pcf_charsets.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+from pathlib import PosixPath
 
 import pytest
 
@@ -14,38 +15,40 @@ from .helper import (
 
 fontname = "Tests/fonts/ter-x20b.pcf"
 
-charsets = {
-    "iso8859-1": {
-        "glyph_count": 223,
-        "message": "hello, world",
-        "image1": "Tests/images/test_draw_pbm_ter_en_target.png",
-    },
-    "iso8859-2": {
-        "glyph_count": 223,
-        "message": "witaj świecie",
-        "image1": "Tests/images/test_draw_pbm_ter_pl_target.png",
-    },
-    "cp1250": {
-        "glyph_count": 250,
-        "message": "witaj świecie",
-        "image1": "Tests/images/test_draw_pbm_ter_pl_target.png",
-    },
+charsets: dict[str, tuple[int, str, str]] = {
+    "iso8859-1": (
+        223,
+        "hello, world",
+        "Tests/images/test_draw_pbm_ter_en_target.png",
+    ),
+    "iso8859-2": (
+        223,
+        "witaj świecie",
+        "Tests/images/test_draw_pbm_ter_pl_target.png",
+    ),
+    "cp1250": (
+        250,
+        "witaj świecie",
+        "Tests/images/test_draw_pbm_ter_pl_target.png",
+    ),
 }
 
 
 pytestmark = skip_unless_feature("zlib")
 
 
-def save_font(request, tmp_path, encoding):
+def save_font(
+    request: pytest.FixtureRequest, tmp_path: PosixPath, encoding: str
+) -> str:
     with open(fontname, "rb") as test_file:
         font = PcfFontFile.PcfFontFile(test_file, encoding)
     assert isinstance(font, FontFile.FontFile)
     # check the number of characters in the font
-    assert len([_f for _f in font.glyph if _f]) == charsets[encoding]["glyph_count"]
+    assert len([_f for _f in font.glyph if _f]) == charsets[encoding][0]
 
     tempname = str(tmp_path / "temp.pil")
 
-    def delete_tempfile():
+    def delete_tempfile() -> None:
         try:
             os.remove(tempname[:-4] + ".pbm")
         except OSError:
@@ -64,23 +67,29 @@ def save_font(request, tmp_path, encoding):
 
 
 @pytest.mark.parametrize("encoding", ("iso8859-1", "iso8859-2", "cp1250"))
-def test_sanity(request, tmp_path, encoding):
+def test_sanity(
+    request: pytest.FixtureRequest, tmp_path: PosixPath, encoding: str
+) -> None:
     save_font(request, tmp_path, encoding)
 
 
 @pytest.mark.parametrize("encoding", ("iso8859-1", "iso8859-2", "cp1250"))
-def test_draw(request, tmp_path, encoding):
+def test_draw(
+    request: pytest.FixtureRequest, tmp_path: PosixPath, encoding: str
+) -> None:
     tempname = save_font(request, tmp_path, encoding)
     font = ImageFont.load(tempname)
     im = Image.new("L", (150, 30), "white")
     draw = ImageDraw.Draw(im)
-    message = charsets[encoding]["message"].encode(encoding)
+    message = charsets[encoding][1].encode(encoding)
     draw.text((0, 0), message, "black", font=font)
-    assert_image_similar_tofile(im, charsets[encoding]["image1"], 0)
+    assert_image_similar_tofile(im, charsets[encoding][2], 0)
 
 
 @pytest.mark.parametrize("encoding", ("iso8859-1", "iso8859-2", "cp1250"))
-def test_textsize(request, tmp_path, encoding):
+def test_textsize(
+    request: pytest.FixtureRequest, tmp_path: PosixPath, encoding: str
+) -> None:
     tempname = save_font(request, tmp_path, encoding)
     font = ImageFont.load(tempname)
     for i in range(255):
@@ -90,7 +99,7 @@ def test_textsize(request, tmp_path, encoding):
         assert dy == 20
         assert dx in (0, 10)
         assert font.getlength(bytearray([i])) == dx
-    message = charsets[encoding]["message"].encode(encoding)
+    message = charsets[encoding][1].encode(encoding)
     for i in range(len(message)):
         msg = message[: i + 1]
         assert font.getlength(msg) == len(msg) * 10
