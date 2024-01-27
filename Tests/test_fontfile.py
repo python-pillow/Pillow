737314923fd1abe8cea2b1986626302215436481
Andrew Murray <radarhere@users.noreply.github.com>
2024-01-27 15:19:43 +1100

    Added type hints

--- Tests/test_fontfile.py
+++ Tests/test_fontfile.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 import pytest
 
 from PIL import FontFile
 
 
-def test_save(tmp_path):
+def test_save(tmp_path: Path) -> None:
     tempname = str(tmp_path / "temp.pil")
 
     font = FontFile.FontFile()
