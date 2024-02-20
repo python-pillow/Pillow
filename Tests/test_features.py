ab9dfd8181868922abee46dffb11ffcb9a772958
Nulano <nulano@nulano.eu>
2024-02-20 20:37:33 +0100

    Add sys.{executable,base_prefix,prefix} to features.pilinfo

--- Tests/test_features.py
+++ Tests/test_features.py
@@ -129,9 +129,15 @@ def test_pilinfo() -> None:
     while lines[0].startswith("    "):
         lines = lines[1:]
     assert lines[0] == "-" * 68
-    assert lines[1].startswith("Python modules loaded from ")
-    assert lines[2].startswith("Binary modules loaded from ")
-    assert lines[3] == "-" * 68
+    assert lines[1].startswith("Python executable is")
+    lines = lines[2:]
+    if lines[0].startswith("Environment Python files loaded from"):
+        lines = lines[1:]
+    assert lines[0].startswith("System Python files loaded from")
+    assert lines[1] == "-" * 68
+    assert lines[2].startswith("Python Pillow modules loaded from ")
+    assert lines[3].startswith("Binary Pillow modules loaded from ")
+    assert lines[4] == "-" * 68
     jpeg = (
         "\n"
         + "-" * 68
