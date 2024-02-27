c00d0191861fd43b39e75742b68ae8df7041b67e
Andrew Murray <radarhere@users.noreply.github.com>
2024-02-28 07:05:47 +1100

    Updated openjpeg to 2.5.2

--- depends/install_openjpeg.sh
+++ depends/install_openjpeg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install openjpeg
 
-archive=openjpeg-2.5.0
+archive=openjpeg-2.5.2
 
 ./download-and-extract.sh $archive https://raw.githubusercontent.com/python-pillow/pillow-depends/main/$archive.tar.gz
 
