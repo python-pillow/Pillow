f6086d40cc728f9c153b430e9566550fb5068912
olokelo <olokelo@gmail.com>
2024-03-19 13:00:50 +0100

    modify leak test

--- Tests/test_jxl_leaks.py
+++ Tests/test_jxl_leaks.py
@@ -6,18 +6,16 @@ from PIL import Image
 
 from .helper import PillowLeakTestCase, skip_unless_feature
 
-test_file = "Tests/images/hopper.jxl"
+TEST_FILE = "Tests/images/hopper.jxl"
 
 
 @skip_unless_feature("jxl")
 class TestJxlLeaks(PillowLeakTestCase):
-    # TODO: lower the limit, I'm not sure what is correct limit
-    # since I have libjxl debug system-wide
-    mem_limit = 16 * 1024  # kb
-    iterations = 100
+    mem_limit = 6 * 1024  # kb
+    iterations = 1000
 
     def test_leak_load(self) -> None:
-        with open(test_file, "rb") as f:
+        with open(TEST_FILE, "rb") as f:
             im_data = f.read()
 
         def core() -> None:
