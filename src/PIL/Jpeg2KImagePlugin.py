c61a4810289e6283e2d20cc5d4941f4c57b19a64
Andrew Murray <radarhere@users.noreply.github.com>
2024-04-06 13:40:39 +1100

    Support reading CMYK JPEG2000 images

--- src/PIL/Jpeg2KImagePlugin.py
+++ src/PIL/Jpeg2KImagePlugin.py
@@ -176,6 +176,10 @@ def _parse_jp2_header(fp):
                 mode = "RGB"
             elif nc == 4:
                 mode = "RGBA"
+        elif tbox == b"colr" and nc == 4:
+            meth, _, _, enumcs = header.read_fields(">BBBI")
+            if meth == 1 and enumcs == 12:
+                mode = "CMYK"
         elif tbox == b"pclr" and mode in ("L", "LA"):
             ne, npc = header.read_fields(">HB")
             bitdepths = header.read_fields(">" + ("B" * npc))
