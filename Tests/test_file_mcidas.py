8a11f09e718b1a52637f8908a04c1bdab7d511d7
Deekshu Kare <n.s.d.kare@student.vu.nl>
2024-06-21 23:27:12 +0200

    original test mcidas

--- Tests/test_file_mcidas.py
+++ Tests/test_file_mcidas.py
@@ -6,6 +6,7 @@ from PIL import Image, McIdasImagePlugin
 
 from .helper import assert_image_equal_tofile
 
+
 def test_invalid_file() -> None:
     invalid_file = "Tests/images/flower.jpg"
 
@@ -29,51 +30,3 @@ def test_valid_file() -> None:
         assert im.mode == "I"
         assert im.size == (1800, 400)
         assert_image_equal_tofile(im, saved_file)
-
-
-def test_open_invalid_file() -> None:
-    invalid_file = "Tests/images/drawing_wmf_ref_144.png"
-
-    with pytest.raises(SyntaxError):
-        McIdasImagePlugin.McIdasImageFile(invalid_file)
-
-
-def test_open_8bit_file() -> None:
-    test_file = "Tests/images/l_trns.png"
-
-    with Image.open(test_file) as im:
-        im.load()
-
-        assert im.format == "MCIDAS"
-        assert im.mode == "L"
-        assert im.size == (800, 600)
-
-
-def test_open_16bit_file() -> None:
-    test_file = "Tests/images/rgb_trns.png"
-
-    with Image.open(test_file) as im:
-        im.load()
-
-        assert im.format == "MCIDAS"
-        assert im.mode == "I"
-        assert im.size == (1024, 768)
-
-
-def test_open_32bit_file() -> None:
-    test_file = "Tests/images/tga/common/200x32_la.png"
-
-    with Image.open(test_file) as im:
-        im.load()
-
-        assert im.format == "MCIDAS"
-        assert im.mode == "I"
-        assert im.size == (1280, 1024)
-
-
-def test_open_unsupported_format() -> None:
-    test_file = "Tests/images/exif_text.png"
-
-    with pytest.raises(SyntaxError):
-        McIdasImagePlugin.McIdasImageFile(test_file)
-
