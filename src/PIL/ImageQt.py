01529d8b0979597902d26fcf4c0dc4f3da2a667e
Andrew Murray <radarhere@users.noreply.github.com>
2024-07-15 19:23:36 +1000

    Added type hints

--- src/PIL/ImageQt.py
+++ src/PIL/ImageQt.py
@@ -19,10 +19,13 @@ from __future__ import annotations
 
 import sys
 from io import BytesIO
-from typing import Callable
+from typing import TYPE_CHECKING, Callable
 
 from . import Image
 from ._util import is_path
+
+if TYPE_CHECKING:
+    from . import ImageFile
 
 qt_version: str | None
 qt_versions = [
@@ -90,11 +93,11 @@ def fromqimage(im):
     return Image.open(b)
 
 
-def fromqpixmap(im):
+def fromqpixmap(im) -> ImageFile.ImageFile:
     return fromqimage(im)
 
 
-def align8to32(bytes, width, mode):
+def align8to32(bytes: bytes, width: int, mode: str) -> bytes:
     """
     converts each scanline of data from 8 bit to 32 bit aligned
     """
@@ -172,7 +175,7 @@ def _toqclass_helper(im):
 if qt_is_installed:
 
     class ImageQt(QImage):
-        def __init__(self, im):
+        def __init__(self, im) -> None:
             """
             An PIL image wrapper for Qt.  This is a subclass of PyQt's QImage
             class.
