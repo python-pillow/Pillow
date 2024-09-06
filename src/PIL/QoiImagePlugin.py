06c331af5d85825b7c2595ca3861730d52b22e00
Andrew Murray <radarhere@users.noreply.github.com>
2024-09-06 15:16:59 +1000

    Added type hints to frombuffer

--- src/PIL/QoiImagePlugin.py
+++ src/PIL/QoiImagePlugin.py
@@ -47,7 +47,7 @@ class QoiDecoder(ImageFile.PyDecoder):
         hash_value = (r * 3 + g * 5 + b * 7 + a * 11) % 64
         self._previously_seen_pixels[hash_value] = value
 
-    def decode(self, buffer: bytes) -> tuple[int, int]:
+    def decode(self, buffer: bytes | Image.SupportsArrayInterface) -> tuple[int, int]:
         assert self.fd is not None
 
         self._previously_seen_pixels = {}
