81412212016a70eb160460e26dc552a0f8a8c153
Andrew Murray <radarhere@users.noreply.github.com>
2025-04-01 08:35:19 +1100

    Allow cmake<4 when building libtiff

--- winbuild/build_prepare.py
+++ winbuild/build_prepare.py
@@ -235,6 +235,7 @@ DEPS: dict[str, dict[str, Any]] = {
                 "-DBUILD_SHARED_LIBS:BOOL=OFF",
                 "-DWebP_LIBRARY=libwebp",
                 '-DCMAKE_C_FLAGS="-nologo -DLZMA_API_STATIC"',
+                "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
             )
         ],
         "headers": [r"libtiff\tiff*.h"],
