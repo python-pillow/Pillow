621bcd56f0ecc2a15a361a16f30cea1ef33a7440
Andrew Murray <3112309+radarhere@users.noreply.github.com>
2025-04-08 22:50:48 +1000

    Removed CMAKE_INTERPROCEDURAL_OPTIMIZATION (#37)

--- .github/workflows/wheels-dependencies.sh
+++ .github/workflows/wheels-dependencies.sh
@@ -143,7 +143,6 @@ function build_libavif {
             -DCONFIG_AV1_DECODER=0 \
             -DAVIF_CODEC_AOM_DECODE=OFF \
             -DAVIF_CODEC_DAV1D=LOCAL \
-            -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
             -DCMAKE_BUILD_TYPE=$build_type \
             . \
         && make install)
