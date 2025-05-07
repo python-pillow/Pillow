5e0457a2b39a0538dd044e64336893273b88d3ac
Andrew Murray <radarhere@users.noreply.github.com>
2025-05-07 18:07:40 +1000

    Removed getxmp()

--- src/PIL/JpegXlImagePlugin.py
+++ src/PIL/JpegXlImagePlugin.py
@@ -24,8 +24,8 @@ except ImportError:
 
 
 def _accept(prefix: bytes) -> bool:
-    is_jxl = prefix.startswith(b"\xff\x0a") or prefix.startswith(
-        b"\x00\x00\x00\x0c\x4a\x58\x4c\x20\x0d\x0a\x87\x0a"
+    is_jxl = prefix.startswith(
+        (b"\xff\x0a", b"\x00\x00\x00\x0c\x4a\x58\x4c\x20\x0d\x0a\x87\x0a")
     )
     if is_jxl and not SUPPORTED:
         msg = "image file could not be identified because JXL support not installed"
@@ -85,9 +85,6 @@ class JpegXlImageFile(ImageFile.ImageFile):
         if "exif" not in self.info:
             return None
         return self.getexif()._get_merged_dict()
-
-    def getxmp(self) -> dict[str, str]:
-        return self._getxmp(self.info["xmp"]) if "xmp" in self.info else {}
 
     def _get_next(self) -> tuple[bytes, float, float, bool]:
 
