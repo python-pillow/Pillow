69c0c422c86b1b1a8a7f5fca5e2f464cadfcf7f2
Andrew Murray <3112309+radarhere@users.noreply.github.com>
2025-06-28 10:29:01 +1000

    Increase pytest verbosity (#9040)
    Co-authored-by: Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>

--- .ci/test.cmd
+++ .ci/test.cmd
@@ -1,3 +1,3 @@
 python.exe -c "from PIL import Image"
 IF ERRORLEVEL 1 EXIT /B
-python.exe -bb -m pytest -v -x -W always --cov PIL --cov Tests --cov-report term --cov-report xml Tests
+python.exe -bb -m pytest -vv -x -W always --cov PIL --cov Tests --cov-report term --cov-report xml Tests
