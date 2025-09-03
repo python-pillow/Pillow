8b4a716582629cc334ae5fc8993a42c629c9e90f
Andrew Murray <radarhere@users.noreply.github.com>
2025-09-03 22:23:52 +1000

    Import fuzz_font from oss_fuzz directory

--- Tests/test_font_crash.py
+++ Tests/test_font_crash.py
@@ -2,19 +2,13 @@ from __future__ import annotations
 
 import pytest
 
-from PIL import ImageFont
-
 from .helper import skip_unless_feature
+from .oss_fuzz.fuzzers import fuzz_font
 
 
 class TestFontCrash:
-    def _fuzz_font(self, font: ImageFont.FreeTypeFont) -> None:
-        # from fuzzers.fuzz_font
-        font.getbbox("ABC")
-        font.getmask("test text")
-
     @skip_unless_feature("freetype2")
     def test_segfault(self) -> None:
-        with pytest.raises(OSError):
-            font = ImageFont.truetype("Tests/fonts/fuzz_font-5203009437302784")
-            self._fuzz_font(font)
+        with open("Tests/fonts/fuzz_font-5203009437302784", "rb") as fp:
+            with pytest.raises(OSError):
+                fuzz_font(fp.read())
