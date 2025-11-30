370da461cf5e1226376d7ea591265a84dc5a5b06
Andrew Murray <3112309+radarhere@users.noreply.github.com>
2025-12-01 10:02:09 +1100

    Updated libpng to 1.6.51 (#9305)
    Co-authored-by: Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>

--- winbuild/build_prepare.py
+++ winbuild/build_prepare.py
@@ -121,7 +121,7 @@ V = {
     "LCMS2": "2.17",
     "LIBAVIF": "1.3.0",
     "LIBIMAGEQUANT": "4.4.1",
-    "LIBPNG": "1.6.50",
+    "LIBPNG": "1.6.51",
     "LIBWEBP": "1.6.0",
     "OPENJPEG": "2.5.4",
     "TIFF": "4.7.1",
