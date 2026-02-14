244f7857e061bf285eb2399f6204405e07e13c5a
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2026-02-14 21:44:29 +0200

    Run PillowLeakTestCases separetely without xdist

--- .ci/test.sh
+++ .ci/test.sh
@@ -4,4 +4,5 @@ set -e
 
 python3 -c "from PIL import Image"
 
-python3 -bb -m pytest -vv -x -W always --cov PIL --cov Tests --cov-report term --cov-report xml Tests $REVERSE
+python3 -bb -m pytest -vv -x -W always Tests -m "isolated" -n0
+python3 -bb -m pytest -vv -x -W always Tests -m "not isolated" --cov PIL --cov Tests --cov-report term --cov-report xml $REVERSE
