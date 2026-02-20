5f87e63687041f906552bbee928314488a579b46
Kadir Can Ozden <101993364+bysiber@users.noreply.github.com>
2026-02-20 07:02:16 +0300

    Only close file handle in ImagePalette.save() when we opened it

--- src/PIL/ImagePalette.py
+++ src/PIL/ImagePalette.py
@@ -191,19 +191,24 @@ class ImagePalette:
         if self.rawmode:
             msg = "palette contains raw palette data"
             raise ValueError(msg)
+        close_fp = False
         if isinstance(fp, str):
             fp = open(fp, "w")
-        fp.write("# Palette\n")
-        fp.write(f"# Mode: {self.mode}\n")
-        for i in range(256):
-            fp.write(f"{i}")
-            for j in range(i * len(self.mode), (i + 1) * len(self.mode)):
-                try:
-                    fp.write(f" {self.palette[j]}")
-                except IndexError:
-                    fp.write(" 0")
-            fp.write("\n")
-        fp.close()
+            close_fp = True
+        try:
+            fp.write("# Palette\n")
+            fp.write(f"# Mode: {self.mode}\n")
+            for i in range(256):
+                fp.write(f"{i}")
+                for j in range(i * len(self.mode), (i + 1) * len(self.mode)):
+                    try:
+                        fp.write(f" {self.palette[j]}")
+                    except IndexError:
+                        fp.write(" 0")
+                fp.write("\n")
+        finally:
+            if close_fp:
+                fp.close()
 
 
 # --------------------------------------------------------------------
