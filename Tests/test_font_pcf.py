04470d5151069736f136210d3170bdab402baa47
Andrew Murray <3112309+radarhere@users.noreply.github.com>
2026-03-03 22:51:41 +1100

    Removed unused argument
    Co-authored-by: Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>

--- Tests/test_font_pcf.py
+++ Tests/test_font_pcf.py
@@ -75,7 +75,7 @@ def test_draw(request: pytest.FixtureRequest, tmp_path: Path) -> None:
     assert_image_equal_tofile(im, "Tests/images/test_draw_pbm_target.png")
 
 
-def test_to_imagefont(tmp_path: Path) -> None:
+def test_to_imagefont() -> None:
     with open(fontname, "rb") as test_file:
         pcffont = PcfFontFile.PcfFontFile(test_file)
     imagefont = pcffont.to_imagefont()
