6eb4b5583c766e511e5bac43c292f2e5f8630c8b
Aarni Koskela <akx@iki.fi>
2026-07-03 14:28:16 +0300

    Remove unused parametrization from test_consistency_*

--- Tests/test_image_filter.py
+++ Tests/test_image_filter.py
@@ -172,8 +172,7 @@ def test_kernel_not_enough_coefficients() -> None:
         ImageFilter.Kernel((3, 3), (0, 0))
 
 
-@pytest.mark.parametrize("mode", MODES)
-def test_consistency_3x3(mode: str) -> None:
+def test_consistency_3x3() -> None:
     matrix = (
         -1, -1,  0,
         -1,  0,  1,
@@ -185,8 +184,7 @@ def test_consistency_3x3(mode: str) -> None:
             assert_image_equal(source.filter(kernel), reference)
 
 
-@pytest.mark.parametrize("mode", MODES)
-def test_consistency_5x5(mode: str) -> None:
+def test_consistency_5x5() -> None:
     matrix = (
         -1, -1, -1, -1,  0,
         -1, -1, -1,  0,  1,
