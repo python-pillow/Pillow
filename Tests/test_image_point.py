6f73d148776877e0b256a2d4ac277654b76e73e8
Aarni Koskela <akx@iki.fi>
2026-07-03 22:46:15 +0300

    Add test coverage for more Point modes

--- Tests/test_image_point.py
+++ Tests/test_image_point.py
@@ -39,6 +39,19 @@ def test_sanity() -> None:
         im.point(lambda x: x // 2)
 
 
+@pytest.mark.parametrize("mode", ("LA", "RGBA", "CMYK"))
+def test_multiband(mode: str) -> None:
+    # Exercises im_point_2x8_2x8 (2 bands) and im_point_4x8_4x8 (4 bands)
+    im = hopper(mode)
+    lut = [(x + 1) % 256 for x in range(256)] * len(im.getbands())
+    out = im.point(lut)
+
+    assert out.mode == mode
+    assert isinstance(px := im.getpixel((0, 0)), tuple)  # Placate mypy
+    expected_px = tuple((v + 1) % 256 for v in px)
+    assert out.getpixel((0, 0)) == expected_px
+
+
 def test_16bit_lut() -> None:
     """Tests for 16 bit -> 8 bit lut for converting I->L images
     see https://github.com/python-pillow/Pillow/issues/440
