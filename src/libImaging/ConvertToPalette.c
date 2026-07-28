09aef9f17b26ea34f79b8fdee3b86dbf6ab2b3ab
Aarni Koskela <akx@iki.fi>
2026-07-28 12:11:01 +0300

    Split topalette converter functions for less nesting

--- src/libImaging/ConvertToPalette.c
+++ src/libImaging/ConvertToPalette.c
@@ -11,30 +11,177 @@
 
 #include "Imaging.h"
 
-// TODO: copied from Convert.c, will be removed soon
-static void
-l2rgb(UINT8 *out, const UINT8 *in, int xsize) {
-    int x;
-    for (x = 0; x < xsize; x++) {
-        UINT8 v = *in++;
-        *out++ = v;
-        *out++ = v;
-        *out++ = v;
-        *out++ = 255;
-    }
-}
-
 #if defined(_MSC_VER)
 #pragma optimize("", off)
 #endif
+
+/**
+ * Internal function to convert a grayscale image to a palette image.
+ * The palette is assumed to be the grayscale ramp, so we can just copy the data as is.
+ * imOut and imIn MUST be different images.
+ */
+static void
+topalette_grayscale(Imaging imOut, Imaging imIn) {
+    int alpha = imOut->mode == IMAGING_MODE_PA;
+    /* Grayscale palette: copy data as is */
+    ImagingSectionCookie cookie;
+    ImagingSectionEnter(&cookie);
+    int xsize = imIn->xsize, ysize = imIn->ysize;
+    if (alpha) {
+        for (int y = 0; y < ysize; y++) {
+            // Restrict safe: we know imOut and imIn to be different images
+            UINT8 *restrict in = (UINT8 *)imIn->image[y];
+            UINT8 *restrict out = (UINT8 *)imOut->image[y];
+            for (int x = 0; x < xsize; x++) {
+                UINT8 v = *in++;
+                *out++ = v;
+                *out++ = v;
+                *out++ = v;
+                *out++ = 255;
+            }
+        }
+    } else {
+        for (int y = 0; y < imIn->ysize; y++) {
+            memcpy(imOut->image[y], imIn->image[y], imIn->linesize);
+        }
+    }
+    ImagingSectionLeave(&cookie);
+}
+
+/**
+ * Internal function to convert a colour image to a palette image using
+ * the Floyd-Steinberg dithering algorithm.
+ * imOut and imIn MUST be different images.
+ * @return 0 on success, 1 on memory allocation failure (PyErr is set).
+ */
+static int
+topalette_colour_floyd_steinberg(Imaging imOut, Imaging imIn, ImagingPalette palette) {
+    int alpha = imOut->mode == IMAGING_MODE_PA;
+    int *errors = calloc(imIn->xsize + 1, sizeof(int) * 3);
+    if (!errors) {
+        ImagingError_MemoryError();  // Sets the exception
+        return 1;
+    }
+
+    /* Map each pixel to the nearest palette entry */
+
+    ImagingSectionCookie cookie;
+    ImagingSectionEnter(&cookie);
+    for (int y = 0; y < imIn->ysize; y++) {
+        int r, r0, r1, r2;
+        int g, g0, g1, g2;
+        int b, b0, b1, b2;
+        UINT8 *in = (UINT8 *)imIn->image[y];
+        UINT8 *out = alpha ? (UINT8 *)imOut->image32[y] : imOut->image8[y];
+        int *e = errors;
+
+        r = r0 = r1 = 0;
+        g = g0 = g1 = 0;
+        b = b0 = b1 = b2 = 0;
+
+        for (int x = 0; x < imIn->xsize; x++, in += 4) {
+            int d2;
+            INT16 *cache;
+
+            r = CLIP8(in[0] + (r + e[3 + 0]) / 16);
+            g = CLIP8(in[1] + (g + e[3 + 1]) / 16);
+            b = CLIP8(in[2] + (b + e[3 + 2]) / 16);
+
+            /* get closest colour */
+            cache = &ImagingPaletteCache(palette, r, g, b);
+            if (cache[0] == 0x100) {
+                ImagingPaletteCacheUpdate(palette, r, g, b);
+            }
+            if (alpha) {
+                out[x * 4] = out[x * 4 + 1] = out[x * 4 + 2] = (UINT8)cache[0];
+                out[x * 4 + 3] = 255;
+            } else {
+                out[x] = (UINT8)cache[0];
+            }
+
+            r -= (int)palette->palette[cache[0] * 4];
+            g -= (int)palette->palette[cache[0] * 4 + 1];
+            b -= (int)palette->palette[cache[0] * 4 + 2];
+
+            /* propagate errors (don't ask ;-) */
+            r2 = r;
+            d2 = r + r;
+            r += d2;
+            e[0] = r + r0;
+            r += d2;
+            r0 = r + r1;
+            r1 = r2;
+            r += d2;
+            g2 = g;
+            d2 = g + g;
+            g += d2;
+            e[1] = g + g0;
+            g += d2;
+            g0 = g + g1;
+            g1 = g2;
+            g += d2;
+            b2 = b;
+            d2 = b + b;
+            b += d2;
+            e[2] = b + b0;
+            b += d2;
+            b0 = b + b1;
+            b1 = b2;
+            b += d2;
+
+            e += 3;
+        }
+
+        e[0] = b0;
+        e[1] = b1;
+        e[2] = b2;
+    }
+    ImagingSectionLeave(&cookie);
+    free(errors);
+    return 0;
+}
+
+/**
+ * Internal function to convert a colour image to a palette image using the closest
+ * colour. imOut and imIn MUST be different images.
+ */
+static void
+topalette_colour_closest(Imaging imOut, Imaging imIn, ImagingPalette palette) {
+    int alpha = imOut->mode == IMAGING_MODE_PA;
+    ImagingSectionCookie cookie;
+    ImagingSectionEnter(&cookie);
+    for (int y = 0; y < imIn->ysize; y++) {
+        int r, g, b;
+        UINT8 *in = (UINT8 *)imIn->image[y];
+        UINT8 *out = alpha ? (UINT8 *)imOut->image32[y] : imOut->image8[y];
+
+        for (int x = 0; x < imIn->xsize; x++, in += 4) {
+            INT16 *cache;
+
+            r = in[0];
+            g = in[1];
+            b = in[2];
+
+            /* get closest colour */
+            cache = &ImagingPaletteCache(palette, r, g, b);
+            if (cache[0] == 0x100) {
+                ImagingPaletteCacheUpdate(palette, r, g, b);
+            }
+            if (alpha) {
+                out[x * 4] = out[x * 4 + 1] = out[x * 4 + 2] = (UINT8)cache[0];
+                out[x * 4 + 3] = 255;
+            } else {
+                out[x] = (UINT8)cache[0];
+            }
+        }
+    }
+    ImagingSectionLeave(&cookie);
+}
 
 Imaging
 topalette(
     Imaging imOut, Imaging imIn, const ModeID mode, ImagingPalette inpalette, int dither
 ) {
-    ImagingSectionCookie cookie;
-    int alpha;
-    int x, y;
     ImagingPalette palette = inpalette;
 
     /* Map L or RGB/RGBX/RGBA/RGBa to palette image */
@@ -44,16 +191,13 @@ topalette(
         return (Imaging)ImagingError_ValueError("conversion not supported");
     }
 
-    alpha = mode == IMAGING_MODE_PA;
-
     if (palette == NULL) {
         /* FIXME: make user configurable */
         if (imIn->bands == 1) {
             palette = ImagingPaletteNew(IMAGING_MODE_RGB);
 
             palette->size = 256;
-            int i;
-            for (i = 0; i < 256; i++) {
+            for (int i = 0; i < 256; i++) {
                 palette->palette[i * 4] = palette->palette[i * 4 + 1] =
                     palette->palette[i * 4 + 2] = (UINT8)i;
             }
@@ -78,19 +222,7 @@ topalette(
     imOut->palette = ImagingPaletteDuplicate(palette);
 
     if (imIn->bands == 1) {
-        /* grayscale image */
-
-        /* Grayscale palette: copy data as is */
-        ImagingSectionEnter(&cookie);
-        for (y = 0; y < imIn->ysize; y++) {
-            if (alpha) {
-                l2rgb((UINT8 *)imOut->image[y], (UINT8 *)imIn->image[y], imIn->xsize);
-            } else {
-                memcpy(imOut->image[y], imIn->image[y], imIn->linesize);
-            }
-        }
-        ImagingSectionLeave(&cookie);
-
+        topalette_grayscale(imOut, imIn);
     } else {
         /* colour image */
 
@@ -104,125 +236,22 @@ topalette(
         }
 
         if (dither) {
-            /* floyd-steinberg dither */
-
-            int *errors;
-            errors = calloc(imIn->xsize + 1, sizeof(int) * 3);
-            if (!errors) {
+            if (topalette_colour_floyd_steinberg(imOut, imIn, palette) != 0) {
+                // Exception will have been set by the work function
                 ImagingDelete(imOut);
-                return ImagingError_MemoryError();
+                return NULL;
             }
-
-            /* Map each pixel to the nearest palette entry */
-            ImagingSectionEnter(&cookie);
-            for (y = 0; y < imIn->ysize; y++) {
-                int r, r0, r1, r2;
-                int g, g0, g1, g2;
-                int b, b0, b1, b2;
-                UINT8 *in = (UINT8 *)imIn->image[y];
-                UINT8 *out = alpha ? (UINT8 *)imOut->image32[y] : imOut->image8[y];
-                int *e = errors;
-
-                r = r0 = r1 = 0;
-                g = g0 = g1 = 0;
-                b = b0 = b1 = b2 = 0;
-
-                for (x = 0; x < imIn->xsize; x++, in += 4) {
-                    int d2;
-                    INT16 *cache;
-
-                    r = CLIP8(in[0] + (r + e[3 + 0]) / 16);
-                    g = CLIP8(in[1] + (g + e[3 + 1]) / 16);
-                    b = CLIP8(in[2] + (b + e[3 + 2]) / 16);
-
-                    /* get closest colour */
-                    cache = &ImagingPaletteCache(palette, r, g, b);
-                    if (cache[0] == 0x100) {
-                        ImagingPaletteCacheUpdate(palette, r, g, b);
-                    }
-                    if (alpha) {
-                        out[x * 4] = out[x * 4 + 1] = out[x * 4 + 2] = (UINT8)cache[0];
-                        out[x * 4 + 3] = 255;
-                    } else {
-                        out[x] = (UINT8)cache[0];
-                    }
-
-                    r -= (int)palette->palette[cache[0] * 4];
-                    g -= (int)palette->palette[cache[0] * 4 + 1];
-                    b -= (int)palette->palette[cache[0] * 4 + 2];
-
-                    /* propagate errors (don't ask ;-) */
-                    r2 = r;
-                    d2 = r + r;
-                    r += d2;
-                    e[0] = r + r0;
-                    r += d2;
-                    r0 = r + r1;
-                    r1 = r2;
-                    r += d2;
-                    g2 = g;
-                    d2 = g + g;
-                    g += d2;
-                    e[1] = g + g0;
-                    g += d2;
-                    g0 = g + g1;
-                    g1 = g2;
-                    g += d2;
-                    b2 = b;
-                    d2 = b + b;
-                    b += d2;
-                    e[2] = b + b0;
-                    b += d2;
-                    b0 = b + b1;
-                    b1 = b2;
-                    b += d2;
-
-                    e += 3;
-                }
-
-                e[0] = b0;
-                e[1] = b1;
-                e[2] = b2;
-            }
-            ImagingSectionLeave(&cookie);
-            free(errors);
-
         } else {
-            /* closest colour */
-            ImagingSectionEnter(&cookie);
-            for (y = 0; y < imIn->ysize; y++) {
-                int r, g, b;
-                UINT8 *in = (UINT8 *)imIn->image[y];
-                UINT8 *out = alpha ? (UINT8 *)imOut->image32[y] : imOut->image8[y];
-
-                for (x = 0; x < imIn->xsize; x++, in += 4) {
-                    INT16 *cache;
-
-                    r = in[0];
-                    g = in[1];
-                    b = in[2];
-
-                    /* get closest colour */
-                    cache = &ImagingPaletteCache(palette, r, g, b);
-                    if (cache[0] == 0x100) {
-                        ImagingPaletteCacheUpdate(palette, r, g, b);
-                    }
-                    if (alpha) {
-                        out[x * 4] = out[x * 4 + 1] = out[x * 4 + 2] = (UINT8)cache[0];
-                        out[x * 4 + 3] = 255;
-                    } else {
-                        out[x] = (UINT8)cache[0];
-                    }
-                }
-            }
-            ImagingSectionLeave(&cookie);
+            topalette_colour_closest(imOut, imIn, palette);
         }
         if (inpalette != palette) {
+            // If we created a temporary palette, delete the cache to free memory
             ImagingPaletteCacheDelete(palette);
         }
     }
 
     if (inpalette != palette) {
+        // If we created a temporary palette, delete it to free memory
         ImagingPaletteDelete(palette);
     }
 
