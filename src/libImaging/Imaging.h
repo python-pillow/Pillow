a187e1d667e48afcc1988cb3c39d17b158cb36c5
Aarni Koskela <akx@iki.fi>
2026-07-28 11:42:33 +0300

    Document palette constraints better; use constants

--- src/libImaging/Imaging.h
+++ src/libImaging/Imaging.h
@@ -151,16 +151,17 @@ struct ImagingHistogramInstance {
     long *histogram; /* Histogram (bands*256 longs) */
 };
 
+#define IMAGING_PALETTE_MAX_ENTRIES 256
+
 struct ImagingPaletteInstance {
     /* Format */
-    ModeID mode;
+    ModeID mode; /* RGB/RGBA/CMYK */
 
     /* Data */
     int size;
-    UINT8 palette[1024]; /* Palette data (same format as image data) */
+    UINT8 palette[IMAGING_PALETTE_MAX_ENTRIES * 4]; /* Palette data (see mode) */
 
-    INT16 *cache;   /* Palette cache (used for predefined palettes) */
-    int keep_cache; /* This palette will be reused; keep cache */
+    INT16 *cache; /* Palette cache (used for predefined palettes) */
 };
 
 typedef struct ImagingMemoryArena {
