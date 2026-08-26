68cc8a530a0ef8caba37bb5bfe03f78b12feae32
Aarni Koskela <akx@iki.fi>
2026-08-26 20:55:29 +0300

    Make all arguments for _imagingft.getfont() actually required
    Avoids a segfault when `encoding` and `font_bytes` weren't passed at all (they were left uninitialized)

--- src/_imagingft.c
+++ src/_imagingft.c
@@ -134,8 +134,8 @@ getfont(PyObject *self_, PyObject *args, PyObject *kw) {
     FT_Long width;
     Py_ssize_t index = 0;
     Py_ssize_t layout_engine = 0;
-    unsigned char *encoding;
-    unsigned char *font_bytes;
+    unsigned char *encoding = NULL;
+    unsigned char *font_bytes = NULL;
     Py_ssize_t font_bytes_size = 0;
     static char *kwlist[] = {
         "filename", "size", "index", "encoding", "font_bytes", "layout_engine", NULL
@@ -152,7 +152,7 @@ getfont(PyObject *self_, PyObject *args, PyObject *kw) {
     if (!PyArg_ParseTupleAndKeywords(
             args,
             kw,
-            "etf|nsy#n",
+            "etfnsy#n",
             kwlist,
             config.filesystem_encoding,
             &filename,
@@ -171,7 +171,7 @@ getfont(PyObject *self_, PyObject *args, PyObject *kw) {
     if (!PyArg_ParseTupleAndKeywords(
             args,
             kw,
-            "etf|nsy#n",
+            "etfnsy#n",
             kwlist,
             Py_FileSystemDefaultEncoding,
             &filename,
