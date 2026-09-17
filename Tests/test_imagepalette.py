662afffc6218e7003f14124fc18593aea43d94eb
Andrew Murray <radarhere@users.noreply.github.com>
2026-09-17 20:46:34 +1000

    Raise error when creating ImagePalette with unsupported mode

--- Tests/test_imagepalette.py
+++ Tests/test_imagepalette.py
@@ -31,9 +31,13 @@ def test_colors_rawmode() -> None:
     }
 
 
-def test_load_wrong_mode() -> None:
+def test_wrong_mode() -> None:
+    with pytest.raises(ValueError, match="unsupported palette mode"):
+        ImagePalette.ImagePalette("L")
+
     im = Image.new("L", (1, 1))
-    im.palette = ImagePalette.ImagePalette("L")
+    im.palette = ImagePalette.ImagePalette()
+    im.palette.mode = "L"
     im.palette.dirty = 1
     with pytest.raises(ValueError, match="image has wrong mode"):
         im.load()
--- src/PIL/ImagePalette.py
+++ src/PIL/ImagePalette.py
@@ -36,7 +36,7 @@ class ImagePalette:
     Color palette for palette mapped images
 
     :param mode: The mode to use for the palette. See:
-        :ref:`concept-modes`. Defaults to "RGB"
+        :ref:`concept-modes`. Must be "RGB", "RGBA" or "CMYK". Defaults to "RGB"
     :param palette: An optional palette. If given, it must be a bytearray,
         an array or a list of ints between 0-255. The list must consist of
         all channels for one color followed by the next color (e.g. RGBRGBRGB).
@@ -48,6 +48,10 @@ class ImagePalette:
         mode: str = "RGB",
         palette: Sequence[int] | bytes | bytearray | None = None,
     ) -> None:
+        if mode not in {"RGB", "RGBA", "CMYK"}:
+            msg = "unsupported palette mode"
+            raise ValueError(msg)
+
         self.mode = mode
         self.rawmode: str | None = None  # if set, palette contains raw data
         self.palette = palette or bytearray()
